218b96778cc8a09809a3735402d332661c31c54f
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2023-05-11 15:15:02 +0200

    app: fix the application name

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ require "dsfr/components"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module BetaRailsTemplate
+module Aplypro
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
