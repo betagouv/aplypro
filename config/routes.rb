355b317aa46f916bc38f83d7393b9a8c642de258
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2023-05-17 12:58:00 +0200

    app: add a way to disconnect

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
 
   devise_for :principals, controllers: { omniauth_callbacks: "principals/omniauth_callbacks" }
 
+  devise_scope :principal do
+    delete "sign_out", to: "devise/sessions#destroy", as: :destroy_principal_session
+  end
+
   get "/dashboard", to: "dashboard#index"
 
   root "home#index"
