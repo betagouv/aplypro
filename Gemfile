92a77554ec305cd260ab7649f3b4afabbc3b6756
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2023-07-11 10:18:57 +0200

    gem: pull in sentry

--- Gemfile
+++ Gemfile
@@ -59,6 +59,10 @@ gem "statesman"
 # proudly found elsewhere - breadcrumbs
 gem "breadcrumbs_on_rails"
 
+# error reporting
+gem "sentry-rails"
+gem "sentry-ruby"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[mri mingw x64_mingw]
---
+++ config/initializers/sentry.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+Sentry.init do |config|
+  config.dsn = ENV.fetch("APLYPRO_SENTRY_DSN", nil)
+  config.breadcrumbs_logger = %i[active_support_logger http_logger]
+
+  # Set traces_sample_rate to 1.0 to capture 100%
+  # of transactions for performance monitoring.
+  # We recommend adjusting this value in production.
+  config.traces_sample_rate = 1.0
+  # or
+  config.traces_sampler = lambda do |_context|
+    true
+  end
+end
