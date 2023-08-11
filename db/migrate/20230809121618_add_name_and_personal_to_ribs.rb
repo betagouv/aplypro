45b0a0ef5edf68e725e240edc521dde7b84fee11
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2023-08-11 17:57:29 +0200

    db: fix a tragic migration error
    Not sure how that even went through in the first place

--- db/migrate/20230809121618_add_name_and_personal_to_ribs.rb
+++ db/migrate/20230809121618_add_name_and_personal_to_ribs.rb
@@ -3,8 +3,8 @@
 class AddNameAndPersonalToRibs < ActiveRecord::Migration[7.0]
   def change
     change_table :ribs, bulk: true do |t|
-      t.name :string, null: false
-      t.personal :boolean, default: false, null: false
+      t.string :name, null: false
+      t.boolean :personal, default: false, null: false
     end
   end
 end
