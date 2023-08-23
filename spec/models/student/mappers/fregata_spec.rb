c5b2ef1ddaf4f8bfdc483ff0a1015c016822b190
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2023-08-23 21:52:20 +0200

    mappers: don't crash on irrelevant MEFs

--- spec/models/student/mappers/fregata_spec.rb
+++ spec/models/student/mappers/fregata_spec.rb
@@ -8,8 +8,17 @@ require "./spec/support/shared/student_mapper"
 describe Student::Mappers::Fregata do
   subject(:mapper) { described_class }
 
+  let!(:fixture) { Rails.root.join("mock/data/fregata-students.json").read }
   let(:etab) { create(:establishment, :with_masa_principal) }
-  let(:data) { JSON.parse(Rails.root.join("mock/data/fregata-students.json").read) }
+  let(:data) { JSON.parse(fixture) }
+
+  let(:irrelevant) do
+    JSON.parse(fixture).map do |student|
+      student["sectionReference"]["codeMef"] = "-123"
+      student["division"]["id"] = 456
+      student
+    end
+  end
 
   it_behaves_like "a student mapper"
 end
