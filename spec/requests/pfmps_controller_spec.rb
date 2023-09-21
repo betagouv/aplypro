ad53f2a6ef427af6e0571b85081377323ca44719
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2023-09-21 11:12:37 +0200

    pfmps: ensure we restrict the scope of managing PFMPs
    We weren't scoping the PFMps to the class or establishment that's
    currently logged in which is bad, and thankfully fixed.

---
+++ spec/requests/pfmps_controller_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "PfmpsController" do
+  let(:schooling) { create(:schooling) }
+  let(:student) { schooling.student }
+  let(:principal) { create(:principal, establishment: student.classe.establishment) }
+  let(:pfmp) { create(:pfmp, student: student) }
+
+  before do
+    sign_in(principal)
+  end
+
+  describe "GET /pfmp" do
+    before do
+      get class_student_pfmp_path(class_id: student.classe.id, student_id: student.id, id: pfmp.id)
+    end
+
+    it { is_expected.to render_template(:show) }
+
+    context "when trying to access a PFMP from another establishment" do
+      before do
+        schooling = create(:schooling)
+        get class_student_pfmp_path(class_id: schooling.classe.id, student_id: schooling.student.id, id: pfmp.id)
+      end
+
+      it { is_expected.to redirect_to classes_path }
+    end
+  end
+end
