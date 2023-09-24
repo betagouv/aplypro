48f2c27ce5bfa91235d3c1ae1c49bbaface51a57
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2023-09-24 22:38:02 +0200

    payments: fix a validation typo

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Payment do
   subject(:payment) { create(:payment) }
 
   it { is_expected.to belong_to(:pfmp) }
-  it { is_expected.to validate_numericality_of(:amount) }
+  it { is_expected.to validate_numericality_of(:amount).is_greater_than(0) }
 
   describe "transitions" do
     it "starts in created state" do
