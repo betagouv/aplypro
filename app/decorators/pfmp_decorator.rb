a1e02f63e76b4c09a32c1d309dd4755c4293f9cb
Caillou <valentin.shamsnejad@gmail.com>
2023-10-13 13:50:06 +0200

    pfmp: improve the display of states and dates

--- app/decorators/pfmp_decorator.rb
+++ app/decorators/pfmp_decorator.rb
@@ -17,10 +17,22 @@ module PfmpDecorator
     t("pfmps.state.#{current_state}")
   end
 
+  def all_status_badges
+    t("pfmps.state").map do |status_key, status_string|
+      is_disabled = current_state.to_sym == status_key ? "" : "is_disabled"
+      dsfr_badge(status: PFMP_STATE_MAPPING[status_key], classes: ["fr-badge--sm", is_disabled]) do
+        status_string
+      end
+    end
+  end
+
   def listing_to_s
     [
       status_badge,
-      [l(start_date), l(end_date)].join(" - ")
-    ].join(" ").html_safe
+      [
+        l(start_date, format: :pfmp_listing),
+        l(end_date, format: :pfmp_listing)
+      ].uniq.join(" - ")
+    ].join(" ")
   end
 end
