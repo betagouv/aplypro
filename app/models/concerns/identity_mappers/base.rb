65be93159114d3971e1d662fe51b29cd3acf203b
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2023-10-14 15:20:13 +0100

    auth: remove some dead code

--- app/models/concerns/identity_mappers/base.rb
+++ app/models/concerns/identity_mappers/base.rb
@@ -5,7 +5,6 @@ module IdentityMappers
     attr_accessor :attributes
 
     ACCEPTED_ESTABLISHMENT_TYPES = %w[LYC LP].freeze
-
     FREDURNERESP_MAPPING = %i[uai type category activity tna_sym tty_code tna_code].freeze
     FREDURNE_MAPPING     = %i[uai type category activity uaj tna_sym tty_code tna_code].freeze
 
@@ -58,10 +57,6 @@ module IdentityMappers
       responsibilities
         .pluck(:uai)
         .map { |uai| Establishment.find_or_create_by!(uai: uai) }
-    end
-
-    def create_all_establishments!
-      establishments.filter(&:no_data?).each(&:fetch_data!)
     end
   end
 end
