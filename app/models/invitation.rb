dd82e9d8dc453c0f355cd4af5d3aa76e5f930cf1
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2023-10-17 16:47:17 +0200

    invitation: extend the list of emails we allow

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -7,9 +7,39 @@ class Invitation < ApplicationRecord
   VALID_DOMAINS = %w[
     gouv.fr
     educagri.fr
+    ac-aix-marseille.fr
+    ac-amiens.fr
+    ac-besancon.fr
+    ac-bordeaux.fr
+    ac-normandie.fr
+    ac-clermont.fr
+    ac-corse.fr
+    ac-creteil.fr
+    ac-dijon.fr
+    ac-grenoble.fr
+    ac-guadeloupe.fr
+    ac-guyane.fr
+    ac-reunion.fr
+    ac-lille.fr
+    ac-limoges.fr
+    ac-lyon.fr
+    ac-martinique.fr
+    ac-mayotte.fr
+    ac-montpellier.fr
+    ac-nancy-metz.fr
+    ac-nantes.fr
+    ac-nice.fr
+    ac-orleans-tours.fr
+    ac-paris.fr
+    ac-poitiers.fr
+    ac-reims.fr
+    ac-rennes.fr
+    ac-strasbourg.fr
+    ac-toulouse.fr
+    ac-versailles.fr
   ].freeze
 
-  VALID_EMAILS = /\.(#{VALID_DOMAINS.map { |d| Regexp.escape(d) }.join('|')})\Z/
+  VALID_EMAILS = /[\.@](#{VALID_DOMAINS.map { |d| Regexp.escape(d) }.join('|')})\Z/
 
   validates :email,
             presence: true, format: { with: VALID_EMAILS },
