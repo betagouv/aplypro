7df825e3b3bec58f070c0d1f3d354976c4f24a88
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2023-10-30 14:34:09 +0100

    invitations: can destroy an invitation

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class InvitationsController < ApplicationController
+  before_action :check_authorisation
+  before_action :set_invitation, only: :destroy
+
   def index
     @invitations = @etab.invitations
 
@@ -19,10 +22,16 @@ class InvitationsController < ApplicationController
     @invitation = Invitation.new(invitation_params)
 
     if @invitation.save
-      redirect_to establishment_invitations_path, notice: "L'email #{@invitation.email} est maintenant autorisé"
+      redirect_to establishment_invitations_path, notice: t("flash.invites.created", email: @invitation.email)
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @invitation.destroy
+
+    redirect_to establishment_invitations_path(@etab), notice: t("flash.invites.destroyed", email: @invitation.email)
   end
 
   private
@@ -32,5 +41,19 @@ class InvitationsController < ApplicationController
       .require(:invitation)
       .permit(:email)
       .with_defaults(user: current_user, establishment: @etab)
+  end
+
+  def set_invitation
+    @invitation = @etab.invitations.find(params[:id])
+  end
+
+  def check_authorisation
+    if !current_user.can_authorise?
+      redirect_back_or_to(
+        root_path,
+        alert: t("flash.pfmps.not_authorised_to_authorise"),
+        status: :forbidden
+      ) and return
+    end
   end
 end
