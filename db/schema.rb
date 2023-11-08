0fc8727badae35f5d0b869f8f1433a34c210cd87
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2023-11-08 17:29:10 +0100

    debug: store oidc attributes in user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_03_103938) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_08_161014) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -203,6 +203,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_03_103938) do
     t.string "last_sign_in_ip"
     t.bigint "establishment_id"
     t.boolean "welcomed", default: false, null: false
+    t.jsonb "oidc_attributes"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["establishment_id"], name: "index_users_on_establishment_id"
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
