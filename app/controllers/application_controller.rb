eb1ab857891435b70604d9970329eb22127acc90
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2023-11-11 17:53:35 +0100

    controllers: put everyone behind the authenticated controller
    You can't do much unauthorised but the app will crash on
    `current_user` rather than redirect.
    
    Closes #134.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :check_maintenance, :set_establishment
+  before_action :authenticate_user!, :check_maintenance, :set_establishment
 
   def after_sign_in_path_for(_resource)
     classes_path
