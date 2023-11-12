2e72465851f25170fedef48c4230a07801f6a1d5
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2023-11-12 23:12:13 +0100

    student mapper: better logic around a student leaving
    You're not in a class if:
    
    * you've changed class;
    * you've changed establishment.
    
    And probably other things, but for now define those two conditions
    separately and leverage the FREGATA attribute for that.

--- spec/models/student/mappers/fregata_spec.rb
+++ spec/models/student/mappers/fregata_spec.rb
@@ -22,6 +22,6 @@ describe Student::Mappers::Fregata do
     let(:nil_ine_payload) { normal_payload.push(build(:fregata_student, :no_ine)) }
     let(:irrelevant_mefs_payload) { build_list(:fregata_student, 10, :irrelevant) }
     let(:gone_student_payload) { build_list(:fregata_student, 1, :gone, ine: student_ine) }
-    let(:changed_class_student_payload) { build_list(:fregata_student, 1, ine: student_ine, classe_label: "NEW") }
+    let(:changed_class_student_payload) { build_list(:fregata_student, 1, :changed_class, ine: student_ine) }
   end
 end
