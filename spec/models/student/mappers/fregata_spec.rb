67c9a4b7ff43d702a2f1468450b32e00e7a2c064
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2023-11-14 19:02:18 +0100

    fregata: grab the address whilst we grab the students list
    We already have the address when we grab the list, so might as well
    avoid N+1 queries to fetch the address for those particular students.

--- spec/models/student/mappers/fregata_spec.rb
+++ spec/models/student/mappers/fregata_spec.rb
@@ -8,6 +8,11 @@ require "./spec/support/shared/student_mapper"
 describe Student::Mappers::Fregata do
   subject(:mapper) { described_class }
 
+  let(:establishment) { create(:establishment, :with_masa_user) }
+  let(:data) { normal_payload }
+  let(:normal_payload) { build_list(:fregata_student, 2) }
+  let(:student_ine) { normal_payload.first["apprenant"]["ine"] }
+
   it_behaves_like "a student mapper" do
     let(:establishment) { create(:establishment, :with_masa_user) }
     let(:data) { normal_payload }
@@ -16,12 +21,16 @@ describe Student::Mappers::Fregata do
     # let!(:fixture) { Rails.root.join("mock/data/fregata-students.json").read }
     # let(:data) { JSON.parse(fixture) }
 
-    let(:normal_payload) { build_list(:fregata_student, 2) }
     let(:empty_payload) { build_list(:fregata_student, 0) }
-    let(:student_ine) { normal_payload.first["apprenant"]["ine"] }
     let(:nil_ine_payload) { normal_payload.push(build(:fregata_student, :no_ine)) }
     let(:irrelevant_mefs_payload) { build_list(:fregata_student, 10, :irrelevant) }
     let(:gone_student_payload) { build_list(:fregata_student, 1, :gone, ine: student_ine) }
     let(:changed_class_student_payload) { build_list(:fregata_student, 1, ine: student_ine, classe_label: "NEW") }
+  end
+
+  it "also grabs the address" do
+    mapper.new(data, establishment).parse!
+
+    expect(Student.find_by(ine: student_ine)).not_to be_missing_address
   end
 end
