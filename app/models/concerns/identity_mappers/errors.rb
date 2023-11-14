bbcb42133e1796e30deddb9cc4fa74f7ba08578a
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2023-11-14 18:11:55 +0100

    auth: remove limited access check in auth process

--- app/models/concerns/identity_mappers/errors.rb
+++ app/models/concerns/identity_mappers/errors.rb
@@ -36,14 +36,6 @@ module IdentityMappers
       end
     end
 
-    class NoLimitedAccessError < Error
-      attr_reader :attributes
-
-      def initialize(msg = "Not allowed in the private beta")
-        super(msg)
-      end
-    end
-
     class UnallowedPrivateEstablishment < Error
       attr_reader :attributes
 
