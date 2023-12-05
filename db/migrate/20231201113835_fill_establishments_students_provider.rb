0457c1de38e337c1b5885714048d1fe293492e8a
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2023-12-05 12:11:33 +0100

    establishment: fix provider migration

--- db/migrate/20231201113835_fill_establishments_students_provider.rb
+++ db/migrate/20231201113835_fill_establishments_students_provider.rb
@@ -3,6 +3,8 @@
 class FillEstablishmentsStudentsProvider < ActiveRecord::Migration[7.1]
   def up
     Establishment.includes(:users).find_each do |establishment|
+      next if establishment.users.none? || establishment.users.first.provider.nil?
+
       provider = establishment.users.first.provider == "masa" ? :fregata : :sygne
       establishment.update!(students_provider: provider)
     end
