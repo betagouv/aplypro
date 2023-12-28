a2c502d619252e1fef29519980c877f12568dfce
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2023-12-28 15:40:14 +0100

    asp: add a mapper for the BOPs
    BOPs (Budget opérationnel de programme) indicates where to take the
    money from. There are four distinct ones, one per MEF-ministry:
    
    - MENJ public ;
    - MENJ private ;
    - MASA ;
    - Mer.
    
    NOTE: establishments in the Army still deliver MENJ-backed diplomas.
    
    The code assumes the public/private distinction is only valid for MENJ
    diplomas, in which case it checks on the establishment.

---
+++ spec/services/asp/bop_mapper_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe ASP::BopMapper do
+  subject(:mapper) { described_class.to_unite_suivi(ministry: ministry, private_establishment: private?) }
+
+  let(:private?) { false }
+
+  context "with a diploma from MASA" do
+    let(:ministry) { "MASA" }
+
+    it { is_expected.to eq "asp masa" }
+  end
+
+  context "with a diploma from Mer" do
+    let(:ministry) { "mer" }
+
+    it { is_expected.to eq "asp mer" }
+  end
+
+  context "with a diploma from MENJ" do
+    let(:ministry) { "menj" }
+
+    context "when the establishment is private" do
+      let(:private?) { true }
+
+      it "returns the right code" do
+        expect(mapper).to eq "asp menj private"
+      end
+    end
+
+    context "when the establishment is public" do
+      it { is_expected.to eq "asp menj public" }
+    end
+  end
+end
