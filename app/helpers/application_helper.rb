b60dd7a2e910a2fec7b724413dd995679fe5e1be
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2024-01-03 10:42:57 +0100

    validation: validate pfmps per class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def success_badge(status, content)
+    dsfr_badge(status: status, classes: ["fr-badge--sm fr-mb-0"]) do
+      content
+    end
+  end
 end
