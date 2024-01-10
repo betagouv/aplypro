bf990eb3754f7be6e262889b62f296608538a3bf
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2024-01-10 18:16:32 +0100

    asp: add the PersonnePhysique XML entity
    Here's the gameplan: we're going to leverage the Active Model API to
    encode some of the ASP logic into the app rather than doing some
    on-the-fly mapping.
    
    This means backporting a bunch of the SERAPIS logic to plain old Ruby
    objects (albeit extending Active Model) that:
    
    1. declare attributes and possible validations ;
    2. are able to be instantiated/mapped from our Active Record models ;
    3. declare a `to_xml` method that can tack onto an existing builder
    object.
    
    This should enable some form of validation chain to make sure we don't
    send garbage over.

---
+++ app/services/asp/entities/personne_physique.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+module ASP
+  module Entities
+    class PersonnePhysique
+      include ActiveModel::API
+      include ActiveModel::Attributes
+      include ActiveModel::AttributeAssignment
+
+      attribute :titre, :string
+      attribute :sexe, :string
+      attribute :nomusage, :string
+      attribute :nomnaissance, :string
+      attribute :prenom, :string
+      attribute :datenaissance, :string
+      attribute :codeinseepaysnai, :string
+      attribute :codeinseecommune, :string
+
+      validates_presence_of %i[
+        titre
+        sexe
+        nomusage
+        nomnaissance
+        prenom
+        datenaissance
+        codeinseepaysnai
+        codeinseecommune
+      ]
+
+      def self.from_student(student)
+        mapper = ASP::Mappers::StudentMapper.new(student)
+
+        new.tap do |instance|
+          mapped_attributes = attribute_names.index_with { |attr| mapper.send(attr) }
+
+          instance.assign_attributes(mapped_attributes)
+        end
+      end
+
+      def to_xml(document = Nokogiri::XML::Document.new)
+        validate!
+
+        Nokogiri::XML::Builder.with(document) do |xml|
+          xml.personnephysique do
+            xml.titre(titre)
+            xml.prenom(prenom)
+            xml.nomusage(nomusage)
+          end
+        end.to_xml
+      end
+    end
+  end
+end
