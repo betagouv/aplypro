3ee4fd877efc6614d8edf5b385f9966b031f839c
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2024-01-10 18:16:32 +0100

    asp: add the PersonnePhysique XML entity
    Here's the gameplan: we're going to leverage the Active Model API to
    encode some of the ASP logic into the app rather than doing some
    on-the-fly mapping.
    
    This means backporting a bunch of the SERAPIS logic to plain old Ruby
    objects (albeit extending Active Model) that:
    
    1. declare attributes and possible validations ;
    2. are able to be instantiated/mapped from our Active Record models ;
    3. declare a `to_xml` method that can tack onto an existing builder
    object.
    
    This should enable some form of validation chain to make sure we don't
    send garbage over.

---
+++ app/services/asp/mappers/student_mapper.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module ASP
+  module Mappers
+    class StudentMapper
+      MAPPING = {
+        prenom: :first_name,
+        nomusage: :last_name,
+        nomnaissance: :last_name,
+        datenaissance: :birthdate,
+        sexe: :biological_sex,
+        codeinseepaysnai: :birthplace_country_insee_code,
+        codeinseecommune: :birthplace_city_insee_code
+      }.freeze
+
+      # yes, we know. It's 2024 and we're still doing this silly
+      # mapping but we're dealing with legacy systems that aren't
+      # aware yet that this is wrong, nor do our APIs provide any
+      # clues as to what the gender of our students might be.
+      GENDER_FOR_SEX = {
+        male: "MR",
+        female: "MME"
+      }.freeze
+
+      attr_reader :student
+
+      def initialize(student)
+        @student = student
+      end
+
+      MAPPING.each do |name, attr|
+        define_method(name) { student[attr] }
+      end
+
+      def titre
+        GENDER_FOR_SEX[student.biological_sex.to_sym]
+      end
+    end
+  end
+end
