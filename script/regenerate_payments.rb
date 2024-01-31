70fb5b8d91a985ea0035520713a9f33ea970be62
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2024-01-31 11:56:25 +0100

    Script to regenerate payments due to former bad mef amounts

---
+++ script/regenerate_payments.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+# ----------------------------------------
+# check if all PFMPs have only one payment
+pfmps_payments_counts = Pfmp.joins(:payments).group(:"pfmps.id").count
+puts pfmps_payments_counts.values.inject (Hash.new(0)) { |h, e| h[e] += 1 ; h }
+# Résultat en prod : {1=>149956, 2=>41}
+# 41 pfmps avec 2 payments, wtf ?
+
+# ----------------------------------------
+# rubocop:disable Metrics/AbcSize
+# For a given MEF, this method corrects all payments in a list of pfmps
+# Note : It is possible for a student who went through different MEFs to reach each MEF's yearly_cap independently
+#        I remember that it is supposed to be like this from a conversation with the Degesco
+def correct_payments_of_a_student_for_a_mef(pfmps, mef)
+  yearly_cap = mef.wage.yearly_cap
+  daily_rate = mef.wage.daily_rate
+  checked_amount = 0
+
+  pfmps.each do |pfmp|
+    # delete extra payments that should'nt even exist
+    pfmp.payments.drop(1).each(&:destroy) if pfmps.payments.size > 1
+
+    allowance_left = yearly_cap - checked_amount
+    payment = pfmp.payments.first
+
+    computed_amount = [pfmp.day_count * daily_rate, allowance_left].min
+    payment.update(amount: computed_amount) if payment.amount != computed_amount
+    checked_amount += computed_amount
+  end
+end
+
+# ----------------------------------------
+# Get pfmps grouped by students
+all_pfmps_per_student = Pfmp
+                        .joins(:schooling)
+                        .group(:"schoolings.student_id")
+                        .pluck("schoolings.student_id", "array_agg(pfmps.id)")
+
+# Loop over pfmps student by student
+all_pfmps_per_student.each_value do |pfmp_ids|
+  # group pfmps of a student by mef
+  pfmps_grouped_by_mef = Pfmp.where(id: pfmp_ids)
+                             .includes(:mef)
+                             .group(:"mefs.id")
+                             .pluck("mefs.id", "array_agg(pfmps.id)")
+
+  pfmps_grouped_by_mef.each do |mef_id, pfmp_ids_of_mef|
+    pfmps = Pfmp.where(id: pfmp_ids_of_mef).includes(:payments, :mef)
+    mef = Mef.find(mef_id)
+    correct_payments_of_a_student_for_a_mef(pfmps, mef)
+  end
+end
+
+# rubocop:enable Metrics/AbcSize
