c7d845df4758853e883ae4f4c2bcabf98adb2dfe
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2024-02-06 14:42:59 +0100

    payments: add a SendPaymentsJob backed by an ASP::Request model
    The ASP::Request model is fairly simple: it piggybacks Active
    Storage's API to store the ASP XML in an S3 bucket, and references all
    the payments it was created for. It's a tracing model.

---
+++ app/models/asp/request.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+module ASP
+  class Request < ApplicationRecord
+    if Rails.env.production?
+      has_one_attached :file, service: :ovh_asp
+    else
+      has_one_attached :file
+    end
+
+    class << self
+      def with_payments(payments, formatter)
+        instance = create.tap do |obj|
+          obj.attach_asp_file(formatter.new(payments))
+        end
+
+        instance.mark_payments!(payments)
+
+        instance
+      end
+    end
+
+    def send!(server)
+      server.drop_file!(
+        io: file.download,
+        path: file.blob.filename.to_s
+      )
+
+      update!(sent_at: DateTime.now)
+    end
+
+    def mark_payments!(payments)
+      ids = payments.pluck(:id)
+
+      Payment.where(id: ids).update(asp_request_id: id)
+    end
+
+    def attach_asp_file(file)
+      self.file.attach(
+        io: StringIO.new(file.to_xml),
+        content_type: "text/xml",
+        filename: file.filename
+      )
+    end
+  end
+end
