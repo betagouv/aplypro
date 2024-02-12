24bbbe52729cfe11c100d5af4f22a2e8deab213a
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2024-02-12 17:26:10 +0100

    asp: add a PaymentRequest class

---
+++ spec/models/asp/payment_request_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe ASP::PaymentRequest do
+  subject(:asp_payment_request) { create(:asp_payment_request) }
+
+  describe "associations" do
+    it { is_expected.to belong_to(:payment) }
+    it { is_expected.to belong_to(:asp_request) }
+  end
+end
