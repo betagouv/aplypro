49ba49de37860612223b38addb06457f6c771171
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2024-02-13 13:16:16 +0100

    asp: add the integrated state to the ASP::PaymentRequest
    When one of the `identifiants_generes_nps_` file comes back from the
    ASP we know that our file and all the information with it is OK. Then
    later we'll expect the `renvoi_paiements.xml` file to actually inform
    whether the payment has gone through.

--- spec/lib/asp/readers/integrations_file_reader_spec.rb
+++ spec/lib/asp/readers/integrations_file_reader_spec.rb
@@ -6,15 +6,30 @@ describe ASP::Readers::IntegrationsFileReader do
   subject(:reader) { described_class.new(data) }
 
   let(:student) { create(:student) }
+  let(:asp_payment_request) { create(:asp_payment_request, :sent) }
 
   let(:data) do
-    """
+    "
 Numero enregistrement;idIndDoss;idIndTiers;idDoss;numAdmDoss;idPretaDoss;numAdmPrestaDoss;idIndPrestaDoss
-#{student.id};700056261;;700086362;ENPUPLF1POP31X20230;700085962;ENPUPLF1POP31X20230;700056261
+#{asp_payment_request.id};700056261;;700086362;ENPUPLF1POP31X20230;700085962;ENPUPLF1POP31X20230;700056261
 """
   end
 
-  it "updates an existing individual request" do
+  xit "updates an existing individual request" do
     expect { reader.process! }.to change { student.reload.asp_individual_reference }.from(nil).to("700056261")
+  end
+
+  describe "payment request transition" do
+    subject(:request) { asp_payment_request }
+
+    it "fails the associated ASP payment request" do
+      expect { reader.process! }.to change { request.reload.current_state }.from("sent").to("integrated")
+    end
+
+    it "attaches the row as JSON in the transition metadata" do
+      reader.process!
+
+      expect(request.last_transition.metadata["idPretaDoss"]).to eq "700085962"
+    end
   end
 end
