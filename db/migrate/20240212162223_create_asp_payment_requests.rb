46b856b0cc8573646d36126ff382b4ea6cfe9dde
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2024-02-13 17:12:00 +0100

    asp: payment requests aren't immediately attached to an actual request

--- db/migrate/20240212162223_create_asp_payment_requests.rb
+++ db/migrate/20240212162223_create_asp_payment_requests.rb
@@ -3,7 +3,7 @@
 class CreateASPPaymentRequests < ActiveRecord::Migration[7.1]
   def change
     create_table :asp_payment_requests do |t|
-      t.references :asp_request, null: false, foreign_key: true
+      t.references :asp_request, null: true, foreign_key: true
       t.references :payment, null: false, foreign_key: true
 
       t.timestamps
