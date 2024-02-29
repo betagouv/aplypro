500d0a70e2d70194722a5ca5d4da6fd446fde634
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2024-02-29 12:21:21 +0100

    pfmp: remove unused pfmps views, and move pfmps badge helper

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -58,24 +58,6 @@ module HomeHelper
     end
   end
 
-  def status_badge(status)
-    status_level = PfmpDecorator::PFMP_STATE_MAPPING[status]
-    dsfr_badge(status: status_level, classes: ["fr-badge"]) do
-      t("pfmps.states.#{status}")
-    end
-  end
-
-  def pfmp_badge(status, count, **args)
-    count ||= 0
-    return if count.zero? && !args[:display_zero]
-
-    count_tag = content_tag(:div, count, class: "fr-mr-1w")
-
-    content_tag(:div, class: "fr-badge-group no-wrap #{args[:class]}", "aria-label": t("pfmps.states.#{status}")) do
-      safe_join([count_tag, status_badge(status)], " ")
-    end
-  end
-
   def school_year_to_s
     starting_year = ENV.fetch("APLYPRO_SCHOOL_YEAR").to_i
 
