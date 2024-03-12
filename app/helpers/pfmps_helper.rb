93658078106d1366c5d8837444df58179b2a819e
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2024-03-12 16:33:38 +0100

    pfmps_helper: rename methods explicitely

--- app/helpers/pfmps_helper.rb
+++ app/helpers/pfmps_helper.rb
@@ -1,21 +1,21 @@
 # frozen_string_literal: true
 
 module PfmpsHelper
-  def status_badge(status)
+  def pfmp_status_badge(status)
     status_level = PfmpDecorator::PFMP_STATE_MAPPING[status]
     dsfr_badge(status: status_level, classes: ["fr-badge"]) do
       t("pfmps.states.#{status}")
     end
   end
 
-  def pfmp_badge(status, count, **args)
+  def pfmps_status_count_badge(status, count, **args)
     count ||= 0
     return if count.zero? && !args[:display_zero]
 
     count_tag = content_tag(:div, count, class: "fr-mr-1w")
 
     content_tag(:div, class: "fr-badge-group no-wrap #{args[:class]}", "aria-label": t("pfmps.states.#{status}")) do
-      safe_join([count_tag, status_badge(status)], " ")
+      safe_join([count_tag, pfmp_status_badge(status)], " ")
     end
   end
 end
