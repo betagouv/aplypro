d03b83d85dc5c81a8c91a355395dad33b9272827
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2024-03-13 19:09:42 +0100

    pfmp: rework the amount calculation once again
    We've had two bits of code that did roughly the same thing:
    AllowanceChecker and `Pfmp.previously_paid_amount_for_mef` which did
    sensibly the same thing.
    
    Use a single module to do it, and test it in and out.

---
+++ spec/models/concerns/pfmp_amount_calculator_spec.rb
@@ -0,0 +1,81 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+# Rubocop doesn't understand the alias
+# rubocop:disable RSpec/EmptyExampleGroup
+describe PfmpAmountCalculator do
+  subject(:amount) { pfmp.reload.calculate_amount }
+
+  let(:pfmp) { create(:pfmp, day_count: 3) }
+  let(:mef) { create(:mef, daily_rate: 1, yearly_cap: 10) }
+
+  RSpec.configure do |config|
+    config.alias_it_behaves_like_to(:it_calculates, "calculates")
+  end
+
+  RSpec.shared_examples "the original amount" do
+    it { is_expected.to eq 3 }
+  end
+
+  RSpec.shared_examples "the yearly-capped amount" do
+    it { is_expected.to eq 10 }
+  end
+
+  RSpec.shared_examples "a limited amount" do |amount|
+    it { is_expected.to eq amount }
+  end
+
+  before do
+    pfmp.schooling.classe.update!(mef: mef)
+  end
+
+  context "when the PFMP doesn't have a day count" do
+    before { pfmp.update!(day_count: nil) }
+
+    it { is_expected.to be_zero }
+  end
+
+  it_calculates "the original amount"
+
+  context "when the PFMP goes over the yearly cap" do
+    before do
+      pfmp.update!(day_count: 1000)
+    end
+
+    it_calculates "the yearly-capped amount"
+  end
+
+  context "when there is a previous PFMP" do
+    let(:previous) { create(:pfmp, :completed, day_count: 8, created_at: Date.yesterday) }
+
+    context "with another schooling" do
+      let(:schooling) { create(:schooling, :closed, student: pfmp.student) }
+
+      before { previous.update!(schooling: schooling) }
+
+      context "with the same MEF" do
+        before { schooling.classe.update!(mef: mef) }
+
+        it_calculates "a limited amount", 2
+
+        context "when the classe is from another year" do
+          before { schooling.classe.update!(start_year: 2022) }
+
+          it_calculates "the original amount"
+        end
+      end
+
+      context "with another MEF" do
+        it_calculates "the original amount"
+      end
+    end
+
+    context "with that schooling" do
+      before { previous.update!(schooling: pfmp.schooling) }
+
+      it_calculates "a limited amount", 2
+    end
+  end
+end
+# rubocop:enable RSpec/EmptyExampleGroup
