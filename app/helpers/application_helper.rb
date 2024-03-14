58b245a45244dec28d80a5c9467f6b7abd9eda62
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2024-03-14 17:49:23 +0100

    cleanup helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,12 +9,12 @@ module ApplicationHelper
 
   def status_count_badge(badge_method: nil, status: nil, status_string: nil, count: nil, **args)
     count ||= 0
-    return if count.zero? && !args[:display_zero]
+    return if !args[:display_zero] && count.zero?
 
     count_tag = content_tag(:div, count, class: "fr-mr-1w")
     disabled = count.zero? ? "disabled" : ""
 
-    content_tag(:div, class: "fr-badge-group no-wrap #{args[:class]} fr-mb-1w", "aria-label": status_string) do
+    content_tag(:div, class: "fr-badge-group no-wrap fr-mb-1w #{args[:class]}", "aria-label": status_string) do
       safe_join([count_tag, send(badge_method, status, class: disabled)], " ")
     end
   end
