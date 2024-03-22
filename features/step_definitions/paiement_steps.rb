bef611856713157a7dc373e18d937e3677db5164
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2024-03-22 07:05:56 +0100

    features: simplify the ASP files steps

--- features/step_definitions/paiement_steps.rb
+++ features/step_definitions/paiement_steps.rb
@@ -2,6 +2,15 @@
 
 World(ActionView::Helpers::NumberHelper)
 
+# hack
+def last_payment_request_for_name(name)
+  find_student_by_full_name(name)
+    .pfmps
+    .last
+    .payment_requests
+    .last
+end
+
 Quand("la tâche de préparation des paiements démarre") do
   PreparePaymentRequestsJob.perform_later
 end
@@ -40,71 +49,49 @@ Sachantqu("il n'y a pas de fichiers sur le serveur de l'ASP") do
   FileUtils.rm_rf(TEMP_ASP_DIR) && FileUtils.mkdir_p(TEMP_ASP_DIR)
 end
 
-Quand("l'ASP a mis a disposition un fichier {string} contenant :") do |filename, string|
-  destination = File.join("tmp/mock_asp", filename)
-
-  File.write(destination, string)
-end
-
-# rubocop:disable Metrics/AbcSize
-def asp_answers_for_dossier(name, is_successful, reason = "")
-  first_name, last_name = name.split
-  student = Student.find_by(first_name:, last_name:)
-
-  file_name_type = is_successful ? :integrations : :rejects
-  file_type = is_successful ? :asp_integration : :asp_reject
-
-  request = student.pfmps.last.payment_requests.last
-  identifier = request.asp_request.file.filename.to_s.split(".xml").first
-  file_name = FactoryBot.build(:asp_filename, file_name_type, identifier: identifier)
-  file_content = FactoryBot.build(file_type, payment_request: request, reason: reason)
-
-  steps %(
-    Sachant que l'ASP a mis a disposition un fichier "#{file_name}" contenant :
-      """
-      #{file_content}
-      """
-  )
-end
-# rubocop:enable Metrics/AbcSize
-
 Sachantque("l'ASP a rejetté le dossier de {string} avec un motif de {string}") do |name, reason|
-  asp_answers_for_dossier(name, false, reason)
+  request = last_payment_request_for_name(name)
+
+  FactoryBot.create(
+    :asp_reject,
+    payment_request: request,
+    reason: reason,
+    destination: TEMP_ASP_DIR
+  )
 end
 
 Sachantque("l'ASP a accepté le dossier de {string}") do |name|
-  asp_answers_for_dossier(name, true)
-end
+  request = last_payment_request_for_name(name)
 
-def asp_answers_for_payment(name, is_successful)
-  first_name, last_name = name.split
-  student = Student.find_by(first_name:, last_name:)
-  request = student.pfmps.last.payment_requests.last
-  state = is_successful ? :success : :failed
-
-  payment_return_file = FactoryBot.build(
-    :asp_payment_return,
-    state,
-    builder_class: ASP::Builder,
-    payment_request: request
-  )
-
-  filename = FactoryBot.build(:asp_filename, :payments)
-
-  steps %(
-    Sachant que l'ASP a mis a disposition un fichier "#{filename}" contenant :
-      """
-      #{payment_return_file}
-      """
+  FactoryBot.create(
+    :asp_integration,
+    payment_request: request,
+    destination: TEMP_ASP_DIR
   )
 end
 
 Sachantque("l'ASP a liquidé le paiement de {string}") do |name|
-  asp_answers_for_payment(name, true)
+  request = last_payment_request_for_name(name)
+
+  FactoryBot.create(
+    :asp_payment_return,
+    :success,
+    builder_class: ASP::Builder,
+    payment_request: request,
+    destination: TEMP_ASP_DIR
+  )
 end
 
 Sachantque("l'ASP n'a pas pu liquider le paiement de {string}") do |name|
-  asp_answers_for_payment(name, false)
+  request = last_payment_request_for_name(name)
+
+  FactoryBot.create(
+    :asp_payment_return,
+    :failed,
+    builder_class: ASP::Builder,
+    payment_request: request,
+    destination: TEMP_ASP_DIR
+  )
 end
 
 Alors("je peux voir une demande de paiement {string}") do |state|
