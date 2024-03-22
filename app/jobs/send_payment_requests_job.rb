d9f2eaee593c329e298ddf7e48c435e752389c68
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2024-03-22 14:29:00 +0100

    asp: remove a state check that will happen right down the line
    In ASP::Request.create! we try and move the requests to :sent, and the
    only allowed state to transition to ready is effectively :ready so
    this check will happen.

--- app/jobs/send_payment_requests_job.rb
+++ app/jobs/send_payment_requests_job.rb
@@ -4,14 +4,10 @@ class SendPaymentRequestsJob < ApplicationJob
   queue_as :default
 
   def perform(payment_requests)
-    requests = ASP::PaymentRequest
-               .joins(ASP::PaymentRequest.most_recent_transition_join)
-               .where(id: payment_requests)
-
-    raise ASP::Errors::SendingPaymentRequestInWrongState if requests.any? { |req| !req.in_state?(:ready) }
-
     ASP::Request
-      .create!(asp_payment_requests: requests)
+      .create!(asp_payment_requests: payment_requests)
       .send!
+  rescue Statesman::TransitionFailedError
+    raise ASP::Errors::SendingPaymentRequestInWrongState
   end
 end
