55466c6616c7391f193e9680c105e8c1736e4f3e
pskl <hello@pascal.cc>
2024-03-25 12:10:20 +0100

    lint: rubocop -a

--- spec/models/asp/payment_request_spec.rb
+++ spec/models/asp/payment_request_spec.rb
@@ -13,23 +13,23 @@ RSpec.describe ASP::PaymentRequest do
     end
   end
 
-  describe 'single_active_payment_request_per_pfmp validation' do
+  describe "single_active_payment_request_per_pfmp validation" do
     let(:pfmp) { create(:pfmp) }
     let(:new_payment_request) { build(:asp_payment_request, pfmp: pfmp) }
 
-    context 'when creating a new PaymentRequest with an existing active request' do
+    context "when creating a new PaymentRequest with an existing active request" do
       let!(:existing_payment_request) { create(:asp_payment_request, :incomplete, pfmp: pfmp) }
 
-      it 'prevents creating the new request' do
+      it "prevents creating the new request" do
         expect(new_payment_request.valid?).to be false
         expect(new_payment_request.errors[:pfmp_id]).to eq(["There can only be one active payment request per Pfmp."])
       end
     end
 
-    context 'when creating a new PaymentRequest without an existing active request' do
+    context "when creating a new PaymentRequest without an existing active request" do
       let!(:existing_payment_request) { create(:asp_payment_request, :rejected, pfmp: pfmp) }
 
-      it 'allows creating the new request' do
+      it "allows creating the new request" do
         expect(new_payment_request.valid?).to be true
       end
     end
