f6778190dce1c068370e933a8e0d611178461660
pskl <hello@pascal.cc>
2024-03-26 13:16:09 +0100

    fix: address comment reviews, use scope to check for active payment_requests (more efficient than calling predicate on each)

--- spec/models/asp/payment_request_spec.rb
+++ spec/models/asp/payment_request_spec.rb
@@ -3,21 +3,31 @@
 require "rails_helper"
 
 RSpec.describe ASP::PaymentRequest do
+  subject { create(:asp_payment_request) }
+
   describe "associations" do
     it { is_expected.to belong_to(:asp_request).optional }
   end
 
   describe "active?" do
-    it "returns false for state rejected" do
-      expect(create(:asp_payment_request, :rejected).active?).to be false
+    subject { create(:asp_payment_request, state) }
+
+    context "when it is rejected" do
+      let(:state) { :rejected }
+
+      it { is_expected.not_to be_active }
     end
 
-    it "returns false for state unpaid" do
-      expect(create(:asp_payment_request, :unpaid).active?).to be false
+    context "when it is unpaid" do
+      let(:state) { :unpaid }
+
+      it { is_expected.not_to be_active }
     end
 
-    it "returns true for other states" do
-      expect(create(:asp_payment_request, :sent).active?).to be true
+    context "when it is sent" do
+      let(:state) { :sent }
+
+      it { is_expected.to be_active }
     end
   end
 
@@ -30,20 +40,20 @@ RSpec.describe ASP::PaymentRequest do
   describe "single_active_payment_request_per_pfmp validation" do
     let(:new_payment_request) { described_class.new(pfmp: existing_payment_request.pfmp) }
 
-    context "when creating a new PaymentRequest with an existing active request" do
+    context "when creating a new payment request with an existing active request" do
       let(:existing_payment_request) { create(:asp_payment_request, :sent) }
 
       it "prevents creating the new request" do
-        new_payment_request.valid?
-        expect(new_payment_request.errors[:pfmp_id]).to eq(["There can only be one active payment request per Pfmp."])
+        new_payment_request.validate
+        expect(new_payment_request.errors[:base]).to eq(["There can only be one active payment request per Pfmp."])
       end
     end
 
-    context "when creating a new PaymentRequest without an existing active request" do
+    context "when creating a new payment request without an existing active request" do
       let(:existing_payment_request) { create(:asp_payment_request, :rejected) }
 
       it "allows creating the new request" do
-        expect(new_payment_request.valid?).to be true
+        expect(new_payment_request).to be_valid
       end
     end
   end
