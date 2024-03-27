ca185ffbac41dcc14d9c9ce60c539ee481ebc134
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2024-03-27 17:46:53 +0100

    spec: fix the establishment facade spec

--- spec/facades/establishment_facade_spec.rb
+++ spec/facades/establishment_facade_spec.rb
@@ -7,22 +7,27 @@ RSpec.describe EstablishmentFacade do
 
   let(:establishment) { build(:establishment, :sygne_provider) }
 
+  before do
+    payment_requests.each { |pr| pr.schooling.classe.update!(establishment: establishment) }
+  end
+
   describe "#payment_requests_counts" do
     subject(:payment_requests_counts) { establishment_facade.payment_requests_counts }
 
-    let(:classe) { create(:classe, establishment: establishment) }
-    let(:pfmps) { create_list(:pfmp, 10, classe: classe) }
-
     context "when there are no payment requests" do
+      let(:payment_requests) { [] }
+
       it "counts all statuses at zero" do
         expect(payment_requests_counts.values).to all be_zero
       end
     end
 
     context "when there are 2 payment requests pending and 3 ready" do
-      before do
-        pfmps.first(2).each { |pfmp| create(:asp_payment_request, :pending, pfmp: pfmp) }
-        pfmps.first(3).each { |pfmp| create(:asp_payment_request, :ready, pfmp: pfmp) }
+      let(:payment_requests) do
+        [
+          create_list(:asp_payment_request, 2, :pending),
+          create_list(:asp_payment_request, 3, :ready)
+        ].flatten
       end
 
       it "sums the pending and ready counts in the pending key" do
@@ -31,9 +36,11 @@ RSpec.describe EstablishmentFacade do
     end
 
     context "when there are 4 payment requests sent and 5 integrated" do
-      before do
-        pfmps.first(4).each { |pfmp| create(:asp_payment_request, :sent, pfmp: pfmp) }
-        pfmps.first(5).each { |pfmp| create(:asp_payment_request, :integrated, pfmp: pfmp) }
+      let(:payment_requests) do
+        [
+          create_list(:asp_payment_request, 4, :sent),
+          create_list(:asp_payment_request, 5, :integrated)
+        ].flatten
       end
 
       it "sums the sent and integrated counts in the sent key" do
@@ -42,9 +49,9 @@ RSpec.describe EstablishmentFacade do
     end
 
     context "when there is 1 payment request in each state" do
-      before do
-        ASP::PaymentRequestStateMachine.states.each_with_index do |state, index|
-          create(:asp_payment_request, state.to_sym, pfmp: pfmps[index])
+      let(:payment_requests) do
+        ASP::PaymentRequestStateMachine.states.map do |state|
+          create(:asp_payment_request, state.to_sym)
         end
       end
 
