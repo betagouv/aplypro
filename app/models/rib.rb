b6e2f174a4190097b1645e46d00b6e6f4006333e
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2024-03-29 12:28:03 +0100

    ribs: validate RIBs against the SEPA zone
    And normalize them as well by stripping leading and trailing whitespaces.

--- app/models/rib.rb
+++ app/models/rib.rb
@@ -6,8 +6,25 @@ class Rib < ApplicationRecord
   validates :iban, :bic, :name, presence: true
   validates :student_id, uniqueness: { scope: :archived_at }, if: :active?
 
-  # courtesy of gem 'bank-account'
-  validates :iban, iban: true
+  normalizes :bic, with: ->(bic) { bic.strip }
+  normalizes :iban, with: ->(iban) { iban.strip }
+
+  before_validation do
+    self.iban = iban.strip unless iban.nil?
+    self.bic = bic.strip unless bic.nil?
+  end
+
+  # gem 'bank-account' provides the :iban and :rib validation but we
+  # have to override the message because they both (the gem and our
+  # SEPA format validation) add the :invalid error key to the object,
+  # which means we can't choose what message to use if we just rely on
+  # Active Record's default translation system.
+  validates :iban,
+            iban: true,
+            format: {
+              with: /\A(#{Aplypro::SEPA_IBANS.join('|')})/,
+              message: I18n.t("activerecord.errors.models.rib.attributes.iban.sepa")
+            }
   validates :bic, bic: true
 
   scope :active, -> { where.not(archived_at: nil) }
