e5d2111721962defeb226063bfad875baae17fba
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2024-04-05 16:14:45 +0200

    asp: also cover the asp_date formatted date attributes

--- spec/support/shared/types.rb
+++ spec/support/shared/types.rb
@@ -2,6 +2,16 @@
 
 require "rails_helper"
 
+RSpec.shared_examples "an ASP-friendly date attribute" do |attribute:|
+  it "formats the date correctly" do
+    date = Date.new(2024, 10, 23)
+
+    model.send "#{attribute}=", date
+
+    expect(model.send(attribute).to_s).to eq "23/10/2024"
+  end
+end
+
 RSpec.shared_examples "a limited string attribute" do |attribute:, length:|
   it "chops the '#{attribute}' attribute to #{length} characters" do
     model.send "#{attribute}=", Faker::Alphanumeric.alpha(number: length + 1)
