dcc36a1f21e14f9fd574c48050da13809e9c55de
pskl <hello@pascal.cc>
2024-04-11 11:41:11 +0200

    Change query (faster that way)

--- app/facades/validations_facade.rb
+++ app/facades/validations_facade.rb
@@ -8,16 +8,11 @@ class ValidationsFacade
   end
 
   def failed_pfmps
-    establishment.pfmps
-                 .in_state(:validated)
-                 .joins(:payment_requests)
-                 .joins("INNER JOIN asp_payment_request_transitions ON \
-asp_payment_requests.id = asp_payment_request_transitions.asp_payment_request_id")
-                 .where(asp_payment_request_transitions: {
-                          to_state: ASP::PaymentRequestStateMachine::FAILED_STATES,
-                          most_recent: true
-                        })
-                 .includes(:student, payment_requests: :asp_payment_request_transitions)
+    Pfmp.joins(schooling: { classe: :establishment })
+        .where(establishments: { id: establishment.id })
+        .joins(:payment_requests)
+        .merge(ASP::PaymentRequest.in_state(ASP::PaymentRequestStateMachine::FAILED_STATES))
+        .includes(:student, payment_requests: :asp_payment_request_transitions)
   end
 
   def validatable_classes
