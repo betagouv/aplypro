a5f0eee9df95ca312d614425d48784c6984c5d9c
pskl <hello@pascal.cc>
2024-04-12 16:39:59 +0200

    Fix Manager (considering swallowing the Concern too)

--- app/services/pfmp_manager.rb
+++ app/services/pfmp_manager.rb
@@ -11,7 +11,7 @@ class PfmpManager
     raise "A PFMP paid or in the process of being paid cannot have its amount recalculated" unless pfmp.can_be_modified?
 
     ActiveRecord::Base.transaction do
-      pfmp.update!(amount: calculate_amount)
+      pfmp.update!(amount: pfmp.calculate_amount)
       rebalance_following_pfmps!
     end
   end
@@ -23,7 +23,7 @@ class PfmpManager
   private
 
   def rebalance_following_pfmps!
-    pfmp.schooling.pfmps.where("start_date > ?", pfmp.end_date).each do |following_pfmp|
+    pfmp.following_modifiable_pfmps.each do |following_pfmp|
       PfmpManager.new(following_pfmp).recalculate_amounts!
     end
   end
