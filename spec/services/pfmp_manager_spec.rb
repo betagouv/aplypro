4cb9f3666b8af12af9bb79dff4b15fb592f963e5
pskl <hello@pascal.cc>
2024-04-15 12:27:48 +0200

    Add service spec file

--- spec/services/pfmp_manager_spec.rb
+++ spec/services/pfmp_manager_spec.rb
@@ -3,4 +3,13 @@
 require "rails_helper"
 
 describe PfmpManager do
+  subject(:manager) { described_class.new(pfmp) }
+
+  let(:pfmp) { create(:asp_payment_request, :rejected).pfmp }
+
+  describe "#reset_payment_request!" do
+    it "creates a new payment request on the pfmp" do
+      expect { manager.reset_payment_request! }.to change(pfmp.payment_requests, :count).by(1)
+    end
+  end
 end
