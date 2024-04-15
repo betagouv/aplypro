2a6ddeb86889100fe1b5207c6358d731a51347c3
pskl <hello@pascal.cc>
2024-04-15 10:54:49 +0200

    Prevent instantiation of PfmpManager, use ApplicationRecord instead of ActiveRecord::Base

--- app/services/pfmp_manager.rb
+++ app/services/pfmp_manager.rb
@@ -12,7 +12,7 @@ class PfmpManager
   def recalculate_amounts!
     raise "A PFMP paid or in the process of being paid cannot have its amount recalculated" unless pfmp.can_be_modified?
 
-    ActiveRecord::Base.transaction do
+    ApplicationRecord.transaction do
       pfmp.update!(amount: pfmp.calculate_amount)
       rebalance_following_pfmps!
     end
@@ -26,7 +26,7 @@ class PfmpManager
 
   def rebalance_following_pfmps!
     pfmp.following_modifiable_pfmps.each do |following_pfmp|
-      PfmpManager.new(following_pfmp).recalculate_amounts!
+      following_pfmp.update!(amount: following_pfmp.calculate_amount)
     end
   end
 end
