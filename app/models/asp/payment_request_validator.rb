0d4f6d138ea66a7595e964e2073caf843f11473b
pskl <hello@pascal.cc>
2024-04-25 16:28:25 +0200

    Clean up validator

--- app/models/asp/payment_request_validator.rb
+++ app/models/asp/payment_request_validator.rb
@@ -7,14 +7,16 @@ module ASP
     def validate(payment_request)
       @payment_request = payment_request
 
-      check_student(payment_request)
-      check_rib(payment_request)
-      check_pfmp(payment_request)
+      check_student
+      check_attributive_decision
+      check_rib
+      check_pfmp
+      check_duplicates
     end
 
     private
 
-    def check_student(payment_request)
+    def check_student
       unless ASP::StudentFileEligibilityChecker.new(student).ready?
         payment_request.errors.add(:ready_state_validation,
                                    :eligibility)
@@ -23,14 +25,9 @@ module ASP
       payment_request.errors.add(:ready_state_validation, :lives_in_france) unless student.lives_in_france?
 
       payment_request.errors.add(:ready_state_validation, :ine_not_found) if student.ine_not_found
-
-      return if payment_request.schooling.attributive_decision.attached?
-
-      payment_request.errors.add(:ready_state_validation,
-                                 :attributive_decision)
     end
 
-    def check_rib(payment_request)
+    def check_rib
       payment_request.errors.add(:ready_state_validation, :rib) unless student&.rib&.valid?
 
       return unless student.adult_without_personal_rib?
@@ -39,9 +36,19 @@ module ASP
                                  :adult_without_personal_rib)
     end
 
-    def check_pfmp(payment_request)
+    def check_pfmp
       payment_request.errors.add(:ready_state_validation, :pfmp) unless pfmp.valid?
       payment_request.errors.add(:ready_state_validation, :pfmp_amount) unless pfmp.amount.positive?
+    end
+
+    def check_attributive_decision
+      return if payment_request.schooling.attributive_decision.attached?
+
+      payment_request.errors.add(:ready_state_validation,
+                                 :attributive_decision)
+    end
+
+    def check_duplicates
       payment_request.errors.add(:ready_state_validation, :duplicates) if pfmp.duplicates.any? do |p|
         p.in_state?(:validated)
       end
