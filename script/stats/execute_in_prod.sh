efa52f229bbe012f6f93880cc0b2880bd3089aa0
pskl <hello@pascal.cc>
2024-05-27 11:42:43 +0200

    Allow running from diff folder, interrupt stats run in case of errors, remove region name from code

--- script/stats/execute_in_prod.sh
+++ script/stats/execute_in_prod.sh
@@ -1,13 +1,20 @@
+#!/bin/bash
 
-production_name=$1
-ruby_string=$2
-output_file_path=$3
+region_name=$1
+production_name=$2
+ruby_string=$3
+output_file_path=$4
 
 # We have 2 levels of interpolation before reaching the rails runner, so we need 2 "\" in front of the quotes
 escaped_ruby_string=$(echo $ruby_string | sed "s/\"/\\\\\"/g")
 
 echo "Executing '$2' in production. Will store the output in '$output_file_path'"
 
-scalingo -region osc-secnum-fr1 --app $production_name run rails runner \""$escaped_ruby_string"\" | sed "1d" > $output_file_path
+scalingo --region $region_name --app $production_name run rails runner \""$escaped_ruby_string"\" | sed "1d" > $output_file_path
+
+if [ $? -ne 0 ]; then
+  echo "Error: Scalingo command failed. Stopping execution."
+  exit 1
+fi
 
 echo "Done."
--- script/stats/get_global_stats_from_prod.sh
+++ script/stats/get_global_stats_from_prod.sh
@@ -1,5 +1,12 @@
-production_name=$1
-bash ./execute_in_prod.sh $production_name "puts Stats::Main.new.global_data_csv" global_stats.csv
-bash ./execute_in_prod.sh $production_name "puts Stats::Main.new.bops_data_csv" bops_stats.csv
-bash ./execute_in_prod.sh $production_name "puts Stats::Main.new.menj_academies_data_csv" menj_academies_stats.csv
-bash ./execute_in_prod.sh $production_name "puts Stats::Main.new.establishments_data_csv" establishments_stats.csv
+#!/bin/bash
+
+region_name="$1"
+production_name="$2"
+
+# infer script dir to be able to run the script from out of the stats folder too
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+
+bash "$script_dir/execute_in_prod.sh" "$region_name" "$production_name" "puts Stats::Main.new.global_data_csv" "$script_dir/global_stats.csv"
+bash "$script_dir/execute_in_prod.sh" "$region_name" "$production_name" "puts Stats::Main.new.bops_data_csv" "$script_dir/bops_stats.csv"
+bash "$script_dir/execute_in_prod.sh" "$region_name" "$production_name" "puts Stats::Main.new.menj_academies_data_csv" "$script_dir/menj_academies_stats.csv"
+bash "$script_dir/execute_in_prod.sh" "$region_name" "$production_name" "puts Stats::Main.new.establishments_data_csv" "$script_dir/establishments_stats.csv"
