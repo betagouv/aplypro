1a97fb968f339dfe72ba858db7025ac127d07aea
tnicolas1 <thibault.nicolas@ac-clermont.fr>
2024-06-07 10:37:59 +0200

    Introduction d'une variable de session pour l'année scolaire

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -46,7 +46,10 @@ class ApplicationController < ActionController::Base
   end
 
   def selected_school_year
-    @selected_school_year = SchoolYear.find_by(start_year: params[:school_year_id]) || SchoolYear.current
+    @selected_school_year =
+      SchoolYear.find_by(start_year: params[:school_year_id]) ||
+      SchoolYear.find_by(start_year: session[:start_year]) ||
+      SchoolYear.current
   end
 
   def infer_page_title(attrs = {})
