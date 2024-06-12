f108102e3b99385bef4fa4e30b3adfe60919e510
pskl <hello@pascal.cc>
2024-06-12 10:56:24 +0200

    Abrogation generation interface buttons  (#967)
    Closes #959 
    
    - [x] Change needs_abrogated_decision
    - [x] Requests specs
    - [x] Job specs
    - [x] Cucumber spec
    - [x] Check DIR rol on abrog
    - [x] Confirm abrogation partial (confirm action)
    - [x] Check confirmed_director role
    
    ![Screenshot 2024-06-11 at 18 00
    18](https://github.com/betagouv/aplypro/assets/2844875/1a20fae7-cc01-4b5f-aac1-d322d0aa9094)

---
+++ spec/requests/schoolings_controller_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe SchoolingsController do
+  let(:schooling) { create(:schooling, :with_attributive_decision) }
+  let(:student) { schooling.student }
+  let(:user) { create(:user, :director, :with_selected_establishment, establishment: student.classe.establishment) }
+
+  before { sign_in(user) }
+
+  describe "DEL /abrogate_decision" do
+    it "enqueues a job to generate the abrogation document" do
+      expect(GenerateAbrogationDecisionJob).to receive(:perform_now).with(schooling) # rubocop:disable RSpec/MessageSpies
+      delete abrogate_decision_class_schooling_path(class_id: schooling.classe.id, id: schooling.id),
+             params: { confirmed_director: "1" }
+    end
+  end
+end
