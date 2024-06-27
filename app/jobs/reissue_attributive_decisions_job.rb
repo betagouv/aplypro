732bb52e5393cecc01f98928c7c48704e0bd16f2
ac-dylan <dylan.champeme@ac-clermont.fr>
2024-06-27 12:18:50 +0200

    update cucumber test

--- app/jobs/reissue_attributive_decisions_job.rb
+++ app/jobs/reissue_attributive_decisions_job.rb
@@ -2,8 +2,7 @@
 
 class ReissueAttributiveDecisionsJob < ApplicationJob
   def perform(establishment)
-    schoolings = establishment
-                   .schoolings
+    schoolings = establishment.schoolings
 
     schoolings.update_all(generating_attributive_decision: true) # rubocop:disable Rails/SkipsModelValidations
 
