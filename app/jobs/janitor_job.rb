74d702b27577b51f0f7037517e725a78a217eaa0
ac-dylan <dylan.champeme@ac-clermont.fr>
2024-07-05 09:33:07 +0200

    Rubocop fix

--- app/jobs/janitor_job.rb
+++ app/jobs/janitor_job.rb
@@ -1,14 +1,17 @@
 # frozen_string_literal: true
 
-class JanitorJob < ApplicationJob
+require "rails_helper"
 
+class JanitorJob < ApplicationJob
   def perform
     reset_attributive_decision_version_overflow
   end
 
   private
-  def reset_attributive_decision_version_overflow
-    Schooling.where('attributive_decision_version > ?', 9).update_all(attributive_decision_version: 9)
-  end
 
+  def reset_attributive_decision_version_overflow
+    Schooling.where("attributive_decision_version > ?", 9).find_each do |schooling|
+      schooling.update(attributive_decision_version: 9)
+    end
+  end
 end
--- spec/jobs/janitor_job_spec.rb
+++ spec/jobs/janitor_job_spec.rb
@@ -1,27 +1,27 @@
-require 'rails_helper'
+# frozen_string_literal: true
+
+require "rails_helper"
 
 RSpec.describe JanitorJob do
-
-  describe '#reset_attributive_decision_version_overflow' do
-    let!(:schooling_under_9) { create(:schooling, attributive_decision_version: 8) }
-    let!(:schooling_equal_9) { create(:schooling, attributive_decision_version: 9) }
-    let!(:schooling_above_9) { create(:schooling, attributive_decision_version: 10) }
+  describe "#reset_attributive_decision_version_overflow" do
+    let!(:schooling_under_nine) { create(:schooling, attributive_decision_version: 8) }
+    let!(:schooling_equal_nine) { create(:schooling, attributive_decision_version: 9) }
+    let!(:schooling_above_nine) { create(:schooling, attributive_decision_version: 10) }
 
     before do
       described_class.new.send(:reset_attributive_decision_version_overflow)
     end
 
-    it 'does not change the attributive_decision_version of schoolings under 9' do
-      expect(schooling_under_9.reload.attributive_decision_version).to eq(8)
+    it "does not change the attributive_decision_version of schoolings under nine" do
+      expect(schooling_under_nine.reload.attributive_decision_version).to eq(8)
     end
 
-    it 'does not change the attributive_decision_version of schoolings equal to 9' do
-      expect(schooling_equal_9.reload.attributive_decision_version).to eq(9)
+    it "does not change the attributive_decision_version of schoolings equal to 9" do
+      expect(schooling_equal_nine.reload.attributive_decision_version).to eq(9)
     end
 
-    it 'resets the attributive_decision_version of schoolings above 9 to 9' do
-      expect(schooling_above_9.reload.attributive_decision_version).to eq(9)
+    it "resets the attributive_decision_version of schoolings above 9 to 9" do
+      expect(schooling_above_nine.reload.attributive_decision_version).to eq(9)
     end
   end
 end
-# frozen_string_literal: true
