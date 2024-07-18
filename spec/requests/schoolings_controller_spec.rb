76ac0d2aebdea8d7c93e366106910f1c79d970ad
ac-dylan <dylan.champeme@ac-clermont.fr>
2024-07-18 16:58:48 +0200

    wip -> merge test files

--- spec/requests/schoolings_controller_spec.rb
+++ spec/requests/schoolings_controller_spec.rb
@@ -6,8 +6,13 @@ RSpec.describe SchoolingsController do
   let(:student) { schooling.student }
   let(:user) { create(:user, :director, :with_selected_establishment, establishment: student.classe.establishment) }
 
+  let(:schooling) { create(:schooling, :with_attributive_decision) }
+  let(:payment_request) { create(:asp_payment_request, :incomplete_for_missing_abrogation_da) }
+
   before do
     sign_in(user)
+    schooling.pfmps = [payment_request.pfmp]
+    schooling.save!
     Timecop.safe_mode = false
     Timecop.freeze(Date.new(2024, 6, 21))
   end
@@ -23,6 +28,19 @@ RSpec.describe SchoolingsController do
                                                                 class_id: schooling.classe.id,
                                                                 id: schooling.id),
              params: { confirmed_director: "1" }
+    end
+  end
+
+  describe "retry_eligibile_payment_requests" do
+    before do
+      schooling.update!(end_date: Date.parse("2024-06-20"))
+    end
+
+    it "retries eligible payment requests" do
+      delete abrogate_decision_school_year_class_schooling_path(schooling.classe.school_year,
+                                                                class_id: schooling.classe.id, id: schooling.id),
+             params: { confirmed_director: "1" }
+      expect(payment_request.last_transition.metadata).not_to include(I18n.t("activerecord.errors.models.asp/payment_request.attributes.ready_state_validation.needs_abrogated_attributive_decision"))
     end
   end
 end
