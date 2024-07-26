542f425a4040238f74c957d778e5d4821eab69a4
pskl <hello@pascal.cc>
2024-07-26 17:07:34 +0200

    Add code etab + add correct enum versio

--- app/services/asp/mappers/adresse_mapper.rb
+++ app/services/asp/mappers/adresse_mapper.rb
@@ -4,7 +4,7 @@ module ASP
   module Mappers
     class AdresseMapper
       PRINCIPAL_ADDRESS_TYPE = "PRINCIPALE"
-      ABROAD_ADDRESS_TYPE = "ETABLISSEMENT"
+      ABROAD_ADDRESS_TYPE = "ADMIN"
 
       MAPPING = {
         codecominsee: :address_city_insee_code,
