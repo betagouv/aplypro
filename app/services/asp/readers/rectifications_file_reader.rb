135b9637e94b679fb7ad7e05040e8fedbef1a1bd
pskl <hello@pascal.cc>
2024-08-05 14:57:53 +0200

    Implement rejections reader

--- app/services/asp/readers/rectifications_file_reader.rb
+++ app/services/asp/readers/rectifications_file_reader.rb
@@ -5,7 +5,55 @@ require "csv"
 module ASP
   module Readers
     class RectificationsFileReader < Base
-      def process!; end
+      attr_reader :xml, :records
+
+      class Node
+        attr_reader :node
+
+        def initialize(node)
+          @node = node
+        end
+
+        def state
+          node.search("ETATPAIEMENT").text
+        end
+
+        def to_h
+          Hash.from_xml(node.to_s)
+        end
+
+        def asp_prestation_dossier_id
+          node.search("LISTEPRESTADOSS/PRESTADOSS/IDPRESTADOSS").first.text
+        end
+      end
+
+      def initialize(io:, record:)
+        super
+
+        @xml = Nokogiri::XML(io)
+        @records = xml.search("LISTEOR/ORDREREVERSEMENT")
+      end
+
+      def each
+        records.each do |record|
+          yield Node.new(record)
+        end
+      end
+
+      def process!
+        each do |node|
+          request =
+            find_payment_request!(node.asp_prestation_dossier_id)
+          case node.state
+          when "EMIS"
+            request.mark_paid!(node.to_h, record)
+          when "INVALIDE"
+            request.mark_unpaid!(node.to_h, record)
+          else
+            raise "unknown payment state: #{state}"
+          end
+        end
+      end
 
       def find_payment_request!(asp_prestation_dossier_id)
         Pfmp
