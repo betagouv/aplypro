e2d0cc68a69694a7bf4b9d6f554e0016ee08e44d
pskl <hello@pascal.cc>
2024-08-21 17:32:29 +0200

    Remove unused step

--- features/step_definitions/classes_step.rb
+++ features/step_definitions/classes_step.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
-Quand("je me rends sur la page classes") do
-  visit "/year/#{SchoolYear.current.start_year}/classes"
-end
-
 Alors("je peux voir {int} PFMP(s) {string} pour la classe {string}") do |count, state, label|
   within("tr", text: label) do
     step(%(l'indicateur de PFMP "#{state}" affiche #{count}))
