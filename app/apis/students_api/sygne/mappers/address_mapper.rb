a8110589716ce253cedbfa236e70113a819f8d1d
tnicolas1 <thibault.nicolas@ac-clermont.fr>
2024-09-06 09:10:24 +0200

    Réutilisation du système de Mapper d'adresse de FREGATA pour SYGNE

--- app/apis/students_api/sygne/mappers/address_mapper.rb
+++ app/apis/students_api/sygne/mappers/address_mapper.rb
@@ -14,10 +14,6 @@ module StudentsApi
 
           rename_keys(
             codePostal: :address_postal_code,
-            adresseLigne1: :address_line1,
-            adresseLigne2: :address_line2,
-            adresseLigne3: :address_line3,
-            adresseLigne4: :address_line4,
             codePays: :address_country_code,
             codeCommuneInsee: :address_city_insee_code,
             libelleCommune: :address_city,
@@ -25,7 +21,11 @@ module StudentsApi
             inseePaysNaissance: :birthplace_country_insee_code
           )
 
-          address
+          nest :address_line1, %i[adresseLigne1 adresseLigne2]
+          nest :address_line2, %i[adresseLigne3 adresseLigne4]
+
+          map_value :address_line1, ->(hash) { hash.values.compact.join(" ") }
+          map_value :address_line2, ->(hash) { hash.values.compact.join(" ") }
 
           accept_keys %i[
             address_postal_code
@@ -38,24 +38,6 @@ module StudentsApi
             birthplace_city_insee_code
             birthplace_country_insee_code
           ]
-        end
-
-        def address(data)
-          data.tap do
-            data[:address_line1] = if data[:address_line1].nil?
-                                     data[:address_line2]
-                                   else
-                                     "#{data[:address_line1]} #{data[:address_line2]}" unless data[:address_line2].nil?
-                                   end
-
-            data[:address_line2] = if data[:address_line3].nil?
-                                     data[:address_line4]
-                                   elsif data[:address_line4].nil?
-                                     data[:address_line3]
-                                   else
-                                     "#{data[:address_line3]} #{data[:address_line4]}"
-                                   end
-          end
         end
       end
     end
