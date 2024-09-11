fb121a7d12040dcd3eab29beea4cfc8e6f1e6a4b
pskl <hello@pascal.cc>
2024-09-11 15:02:42 +0200

    Import missing fonts, images

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,4 @@
 //= link_tree ../images
-//= link_directory ../stylesheets .css
-
-//= link favicon/favicon.ico
-
+//= link_tree ../fonts
 //= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
+//= link_directory ../stylesheets .css
--- config/importmap.rb
+++ config/importmap.rb
@@ -4,8 +4,8 @@
 pin "chartkick", to: "chartkick.js"
 pin "Chart.bundle", to: "Chart.bundle.js"
 pin "application", preload: true
-pin "@gouvfr/dsfr", to: "dsfr.module.min.js"
-pin "@gouvfr/dsfr-nomodule", to: "dsfr.nomodule.min.js", preload: true
+pin "@gouvfr/dsfr", to: "dsfr/dsfr.module.min.js"
+pin "@gouvfr/dsfr-nomodule", to: "dsfr/dsfr.nomodule.min.js", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
