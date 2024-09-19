bc15bb4ed9e96e999615a52a1227508314937a06
pskl <hello@pascal.cc>
2024-09-19 11:11:16 +0200

    Truncate xml addr attrs to 38 chars (#1103)

--- lib/asp/entities/adresse.rb
+++ lib/asp/entities/adresse.rb
@@ -30,8 +30,8 @@ module ASP
           super
         elsif payment_request.pfmp.in_state?(:rectified)
           new(
-            pointremise: payment_request.student.address_line1,
-            cpltdistribution: payment_request.student.address_line2,
+            pointremise: payment_request.student.address_line1.to_s.slice(0, 38), # Max 38 characters
+            cpltdistribution: payment_request.student.address_line2.slice(0, 38), # Max 38 characters
             codetypeadr: ASP::Mappers::PRINCIPAL_ADDRESS_TYPE,
             codeinseepays: InseeCountryCodeMapper.call(payment_request.student.address_country_code),
             codecominsee: payment_request.student.address_city_insee_code,
