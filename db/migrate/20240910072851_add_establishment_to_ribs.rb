fb54c699620c4649e5fa942190bd8ed739a9f2df
pskl <hello@pascal.cc>
2024-09-20 18:32:12 +0200

    Simplify migration

--- db/migrate/20240910072851_add_establishment_to_ribs.rb
+++ db/migrate/20240910072851_add_establishment_to_ribs.rb
@@ -2,6 +2,12 @@ class AddEstablishmentToRibs < ActiveRecord::Migration[7.2]
   def change
     add_reference :ribs, :establishment, foreign_key: true
 
-    Student.find_each { |student| student.ribs.last.update!(establishment: student.current_schooling.establishment) }
+    # Migrate only students with a rib
+    Student.where.associated(:rib).find_each do |student|
+      etab = student&.establishment
+      next unless etab
+
+      student.rib.update!(establishment: etab)
+    end
   end
 end
