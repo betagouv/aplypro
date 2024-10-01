53f3af8675bcecba3379004a3f665d22050dfef6
pskl <hello@pascal.cc>
2024-10-01 16:34:25 +0200

    Fix main spec by allowing a new trait on the students factory

--- spec/factories/students.rb
+++ spec/factories/students.rb
@@ -8,6 +8,7 @@ FactoryBot.define do
     birthdate { Faker::Date.birthday(min_age: 16, max_age: 20) }
 
     transient do
+      establishment { nil }
       birth_country_code { Faker::Number.number(digits: 5) }
       address_country_code_value { Faker::Number.number(digits: 5) }
     end
@@ -72,8 +73,9 @@ FactoryBot.define do
     end
 
     trait :with_personal_rib do
-      after(:create) do |student|
-        create(:rib, :personal, student: student)
+      after(:create) do |student, evaluator|
+        create(:rib, :personal, student: student,
+                                establishment: evaluator.establishment || student.establishment || create(:establishment)) # rubocop:disable Layout/LineLength
       end
     end
 
