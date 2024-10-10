7db04851139fe2d25b94c40799141c9a3698a1ca
pskl <hello@pascal.cc>
2024-10-10 14:48:07 +0200

    Improve csv + revamp Seeder to upsert all Mefs

--- db/mef_seeder.rb
+++ db/mef_seeder.rb
@@ -2,6 +2,9 @@
 
 require "csv"
 
+# For the introduction of a new SchoolYear
+# First create the new SchoolYear so that SchoolYear.current returns the latest one
+# Then run MefSeeder.seed
 class MefSeeder
   MAPPING = {
     code: "MEF",
@@ -12,10 +15,27 @@ class MefSeeder
   }.freeze
 
   def self.seed
-    logger = ActiveSupport::TaggedLogging.new(Logger.new($stdout))
-    logger.info "[seeds] inserting MEF codes..."
+    Dir.glob(Rails.root.join("data/mefs/*.csv")).each do |file_path|
+      process_file(file_path)
+    end
 
-    data = CSV.read(Rails.root.join("data/mefs/2024_2025.csv"), headers: true)
+    logger.info "[seeds] done inserting MEF codes."
+  end
+
+  private
+
+  def self.process_file(file_path)
+    file_name = File.basename(file_path, ".csv")
+    start_year = file_name.split("_").first.to_i
+
+    school_year = SchoolYear.find_by!(start_year: start_year)
+
+    if school_year.nil?
+      logger.warn "[seeds] School year not found for #{file_name}. Skipping file."
+      return
+    end
+
+    data = CSV.read(file_path, headers: true)
 
     mefs = data.map do |entry|
       attributes = MAPPING.transform_values do |value|
@@ -26,11 +46,11 @@ class MefSeeder
         end
       end
 
-      attributes.merge("school_year_id" => SchoolYear.current.id)
+      attributes.merge("school_year_id" => school_year.id)
     end
 
     Mef.upsert_all(mefs, unique_by: :code) # rubocop:disable Rails/SkipsModelValidations
 
-    logger.info "[seeds] done inserting MEF codes."
+    logger.info "[seeds] Inserted MEF codes for school year #{school_year.start_year}-#{school_year.start_year + 1}."
   end
 end
