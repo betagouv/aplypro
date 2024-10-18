0600beb4292db54dbf4e9162747df0e8c68953de
tnicolas1 <thibault.nicolas@ac-clermont.fr>
2024-10-18 09:50:03 +0200

    Rubocop

--- config/initializers/authorised_establishments.rb
+++ config/initializers/authorised_establishments.rb
@@ -3,6 +3,6 @@
 require "csv"
 
 module Aplypro
- AUTHORISED_ESTABLISHMENTS = CSV.read(Rails.root.join("data/authorised-establishments.csv"),
-                                      headers: true, return_headers: false).to_a
+  AUTHORISED_ESTABLISHMENTS = CSV.read(Rails.root.join("data/authorised-establishments.csv"),
+                                       headers: true, return_headers: false).to_a
 end
