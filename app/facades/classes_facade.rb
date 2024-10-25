8e30143b09db1ebc0a5e10b669e59d54ca316c55
tnicolas1 <166516439+tnicolas1@users.noreply.github.com>
2024-10-25 14:55:22 +0200

    Afficher l'état de la payment_request dans la page de détail de l'élève (#1170)
    ![image](https://github.com/user-attachments/assets/06108dbc-2808-44f6-8afb-d5f45e6e7d52)
    
    ---------
    
    Co-authored-by: pskl <hello@pascal.cc>

--- app/facades/classes_facade.rb
+++ app/facades/classes_facade.rb
@@ -44,21 +44,17 @@ class ClassesFacade
   end
 
   def nb_payment_requests(class_id, states)
-    count = 0
-    states.each do |state|
-      count += payments_requests_by_classe_and_state.dig(class_id, state.to_s) || 0
-    end
-    count
+    ASP::PaymentRequest.latest_per_pfmp
+                       .joins(pfmp: { schooling: :classe })
+                       .where(classes: { id: class_id })
+                       .in_state(states)
+                       .count
   end
 
   private
 
   def pfmps_by_classe_and_state
     @pfmps_by_classe_and_state ||= group_pfmps_by_classe_and_state
-  end
-
-  def payments_requests_by_classe_and_state
-    @payments_requests_by_classe_and_state ||= group_payments_requests_by_classe_and_state
   end
 
   def group_pfmps_by_classe_and_state
@@ -70,22 +66,6 @@ class ClassesFacade
         .group("schoolings.classe_id", "COALESCE(pfmp_transitions.to_state, 'pending')")
         .count
         .each do |(class_id, state), count|
-      counts[class_id] ||= {}
-      counts[class_id][state.presence || "pending"] = count
-    end
-
-    counts
-  end
-
-  def group_payments_requests_by_classe_and_state
-    counts = {}
-
-    ASP::PaymentRequest.joins(:schooling)
-                       .joins("LEFT JOIN asp_payment_request_transitions ON asp_payment_request_transitions.asp_payment_request_id = asp_payment_requests.id AND asp_payment_request_transitions.most_recent = true") # rubocop:disable Layout/LineLength
-                       .where(schoolings: { classe_id: @classes.pluck(:id) })
-                       .group("schoolings.classe_id", "COALESCE(asp_payment_request_transitions.to_state, 'pending')")
-                       .count
-                       .each do |(class_id, state), count|
       counts[class_id] ||= {}
       counts[class_id][state.presence || "pending"] = count
     end
