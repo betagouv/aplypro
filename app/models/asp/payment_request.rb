e6e0ad5fa575e73d289cd3359cc59d82e745ae0b
pskl <hello@pascal.cc>
2024-10-25 16:48:56 +0200

    Change in_state check

--- app/models/asp/payment_request.rb
+++ app/models/asp/payment_request.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module ASP
-  class PaymentRequest < ApplicationRecord
+  class PaymentRequest < ApplicationRecord # rubocop:disable Metrics/ClassLength
     TRANSITION_CLASS = ASP::PaymentRequestTransition
     STATE_MACHINE_CLASS = ASP::PaymentRequestStateMachine
 
@@ -127,7 +127,7 @@ module ASP
     end
 
     def eligible_for_rejected_or_unpaid_auto_retry?
-      return false unless in_state?(:rejected) || in_state?(:unpaid)
+      return false unless in_state?(:rejected, :unpaid)
 
       decorator = ActiveDecorator::Decorator.instance.decorate(self)
       message = in_state?(:rejected) ? decorator.rejected_reason : decorator.unpaid_reason
