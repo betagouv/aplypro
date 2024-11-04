0df1a99242cb6e1de3aae8f83754374b10ddee6d
pskl <hello@pascal.cc>
2024-11-04 23:15:23 +0100

    Fix spec

--- app/facades/validations_facade.rb
+++ app/facades/validations_facade.rb
@@ -10,16 +10,14 @@ class ValidationsFacade
 
   # TODO: this query somehow (cant reproduce) returns p_r also not in failed state
   # the reject at the end is duct tape to prevent that from happening
-  def failed_pfmps_per_payment_request_state # rubocop:disable Metrics/AbcSize
-    pfmps = Pfmp.joins(schooling: { classe: :establishment })
-                .where(establishments: { id: establishment.id })
-                .joins(:payment_requests)
-                .joins("INNER JOIN (#{establishment.latest_per_pfmp.failed.to_sql}) as latest_payment_requests
+  def failed_pfmps_per_payment_request_state
+    subquery = establishment.payment_requests.latest_per_pfmp.failed.to_sql
+    pfmps = Pfmp.joins("INNER JOIN (#{subquery}) as latest_payment_requests
                         ON latest_payment_requests.pfmp_id = pfmps.id")
                 .includes(:student, payment_requests: :asp_payment_request_transitions)
 
     pfmps.group_by { |pfmp| pfmp.latest_payment_request.current_state }
-         .reject { |pfmp_state| ASP::PaymentRequestStateMachine::FAILED_STATES.exclude?(pfmp_state.to_sym) }
+         .reject { |pr_state| ASP::PaymentRequestStateMachine::FAILED_STATES.exclude?(pr_state.to_sym) }
   end
 
   def validatable_classes
