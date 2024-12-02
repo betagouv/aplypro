fca89eed37e43406533369da0e55fb4e4278952b
tnicolas1 <thibault.nicolas@ac-clermont.fr>
2024-12-02 11:10:19 +0100

    Réintroduction d'une date fixe dans la factory de 'schoolings'

--- spec/factories/schoolings.rb
+++ spec/factories/schoolings.rb
@@ -11,7 +11,7 @@ FactoryBot.define do
       schooling.start_date = if schooling.end_date.present?
                                schooling.end_date - 1.month
                              else
-                               Date.yesterday - 1.month
+                               Date.parse("#{SchoolYear.current.start_year}-09-10")
                              end
     end
 
