fd49dd95ff16443800f837d65423f982f798a3a4
tnicolas1 <166516439+tnicolas1@users.noreply.github.com>
2025-01-07 10:40:28 +0100

    Retrait des DA (Partie 1) (#1297)

--- app/jobs/generate_attributive_decisions_job.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class GenerateAttributiveDecisionsJob < ApplicationJob
-  def perform(schoolings)
-    jobs = schoolings.map { |schooling| GenerateAttributiveDecisionJob.new(schooling) }
-
-    ActiveJob.perform_all_later(jobs)
-  end
-end
