b07546b738977ceb80df87ad8f6cc17811deb8e4
pskl <hello@pascal.cc>
2025-02-25 06:16:44 +0100

    Split elements on the 2 lines

--- spec/apis/students_api/sygne/mappers/address_mapper_spec.rb
+++ spec/apis/students_api/sygne/mappers/address_mapper_spec.rb
@@ -9,8 +9,8 @@ describe StudentsApi::Sygne::Mappers::AddressMapper do
 
   let(:expected) do
     {
-      address_line1: [data["adrResidenceEle"]["adresseLigne1"], data["adrResidenceEle"]["adresseLigne2"],
-                      data["adrResidenceEle"]["adresseLigne3"], data["adrResidenceEle"]["adresseLigne4"]].join(" "),
+      address_line1: [data["adrResidenceEle"]["adresseLigne1"], data["adrResidenceEle"]["adresseLigne2"]].join(" "),
+      address_line2: [data["adrResidenceEle"]["adresseLigne3"], data["adrResidenceEle"]["adresseLigne4"]].join(" "),
       address_postal_code: data["adrResidenceEle"]["codePostal"],
       address_city: data["adrResidenceEle"]["libelleCommune"],
       address_city_insee_code: data["adrResidenceEle"]["codeCommuneInsee"],
@@ -27,9 +27,9 @@ describe StudentsApi::Sygne::Mappers::AddressMapper do
   context "when address_line2 is nil" do
     before { data["adrResidenceEle"]["adresseLigne2"] = nil }
 
-    it "maps addresses correctly" do
-      expect(mapper.call(data)[:address_line1]).to eq [data["adrResidenceEle"]["adresseLigne1"],
-                                                       data["adrResidenceEle"]["adresseLigne3"],
+    it "maps addresses correctly" do # rubocop:disable RSpec/MultipleExpectations
+      expect(mapper.call(data)[:address_line1]).to eq data["adrResidenceEle"]["adresseLigne1"]
+      expect(mapper.call(data)[:address_line2]).to eq [data["adrResidenceEle"]["adresseLigne3"],
                                                        data["adrResidenceEle"]["adresseLigne4"]].join(" ")
     end
   end
