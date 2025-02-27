4251258a20455bc2b955b0bb46c9e1c5ec06caa7
tnicolas1 <166516439+tnicolas1@users.noreply.github.com>
2025-02-27 11:13:21 +0100

    Ajout de raisons liées aux coordoonées bancaires pour renvoyer en payement (#1409)

--- app/controllers/ribs_controller.rb
+++ app/controllers/ribs_controller.rb
@@ -33,7 +33,7 @@ class RibsController < ApplicationController # rubocop:disable Metrics/ClassLeng
       @rib = @student.create_new_rib(rib_params)
 
       if @rib.save
-        @student.retry_pfmps_payment_requests!(%w[rib bic paiement])
+        @student.retry_pfmps_payment_requests!(%w[rib bic paiement coordonnées bancaires])
 
         redirect_to student_path(@student), notice: t(".success")
       else
