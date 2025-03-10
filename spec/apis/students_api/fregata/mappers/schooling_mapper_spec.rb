8dc5b669680622ef68bcc81e2ba5f323f7a6d1a4
tnicolas1 <thibault.nicolas@ac-clermont.fr>
2025-03-10 10:42:03 +0100

    Ajout de tests unitaires

---
+++ spec/apis/students_api/fregata/mappers/schooling_mapper_spec.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe StudentsApi::Fregata::Mappers::SchoolingMapper do
+  subject(:mapped) { described_class.new.call(data) }
+
+  let(:data) do
+    build(
+      :fregata_student,
+      ine_value: "123456",
+      dateEntreeFormation: "2024-05-25",
+      dateSortieFormation: "2024-05-30",
+      dateSortieEtablissement: "2024-06-01",
+      status_code: "2501"
+    )
+  end
+
+  it "maps the data correctly" do # rubocop:disable RSpec/ExampleLength, just being nice
+    expected = {
+      ine: "123456",
+      start_date: "2024-05-25",
+      status: :student
+    }
+
+    expect(mapped).to eq expected
+  end
+
+  describe "status" do
+    context "when the student is an apprentice" do
+      let(:data) { build(:fregata_student, :apprentice) }
+
+      it "maps it correctly" do
+        expect(mapped[:status]).to eq :apprentice
+      end
+    end
+
+    context "when then student has an unknown type" do
+      let(:data) { build(:fregata_student, status_code: "FOOBAR") }
+
+      it "raises Student::Mappers::Errors::SchoolingParsingError" do
+        expect { mapped[:status] }.to raise_error Student::Mappers::Errors::SchoolingParsingError
+      end
+    end
+  end
+end
