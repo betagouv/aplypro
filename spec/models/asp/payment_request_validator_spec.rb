eafe500122e6cf00a319de2d3d9fd92637d21720
tnicolas1 <166516439+tnicolas1@users.noreply.github.com>
2025-03-13 05:33:29 +0100

    Supprimer la condition d'arrêt sur les mefs dans le Validator (#1432)

--- spec/models/asp/payment_request_validator_spec.rb
+++ spec/models/asp/payment_request_validator_spec.rb
@@ -264,7 +264,6 @@ RSpec.describe ASP::PaymentRequestValidator do
 
   describe "#validate" do
     it "calls all check methods" do
-      expect(validator).to receive(:check_mef)
       expect(validator).to receive(:check_student)
       expect(validator).to receive(:check_insee_code)
       expect(validator).to receive(:check_address)
