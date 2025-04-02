47905cc78470fd372e67adcf9a5698c4e4281be1
pskl <hello@pascal.cc>
2025-04-02 16:51:05 +0200

    Update RUA client to use KC for auth

--- lib/rua/client.rb
+++ lib/rua/client.rb
@@ -1,30 +1,53 @@
 module Rua
   class Client
-    BASE_URL = "https://pr-api-tst-rac.omogen.in.phm.education.gouv.fr/mesirh/pp/rua/pp-grh-mes/v2"
-    API_KEY = ENV.fetch("APLYPRO_RUA_API_KEY")
+    RUA_KC_URL = ENV.fetch("RUA_KC_URL")
+    RUA_KC_GRANT_TYPE = "client_credentials".freeze
+    RUA_KC_CLIENT_ID = ENV.fetch("RUA_KC_CLIENT_ID")
+    RUA_RESOURCE_BASE_URL = ENV.fetch("RUA_RESOURCE_BASE_URL")
+    RUA_KC_CLIENT_SECRET = ENV.fetch("RUA_KC_CLIENT_SECRET")
 
-    attr_reader :conn
+    attr_reader :resource_connection, :access_token
 
     def initialize
-      @conn = connection
+      @access_token = JSON.parse(auth_connection.post("token", kc_form_params).body)["access_token"]
+      @resource_connection = connection
     end
 
-    def agents
-      conn.get("agents")
+    def agent_info(email)
+      JSON.parse(resource_connection.get("agents", { email: email }).body)
+    end
+
+    def auth_connection
+      Faraday.new(
+        url: RUA_KC_URL,
+        headers: auth_headers
+      )
     end
 
     def connection
       Faraday.new(
-        url: BASE_URL,
+        url: RUA_RESOURCE_BASE_URL,
         params: {},
         headers: headers
       )
     end
 
+    def auth_headers
+      { "Content-Type" => "application/x-www-form-urlencoded" }
+    end
+
     def headers
       {
-        "X-Omogen-Api-Key" => API_KEY,
+        "Authorization" => "Bearer #{access_token}",
         "Content-Type" => "application/json"
+      }
+    end
+
+    def kc_form_params
+      {
+        grant_type: RUA_KC_GRANT_TYPE,
+        client_id: RUA_KC_CLIENT_ID,
+        client_secret: RUA_KC_CLIENT_SECRET
       }
     end
   end
