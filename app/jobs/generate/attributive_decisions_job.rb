5cb3013619b421164543b60d1bcf1bf229ce6c99
pskl <hello@pascal.cc>
2025-04-07 16:43:01 +0200

    Update confirmed director on DA generation

--- app/jobs/generate/attributive_decisions_job.rb
+++ app/jobs/generate/attributive_decisions_job.rb
@@ -2,10 +2,20 @@
 
 module Generate
   class AttributiveDecisionsJob < ApplicationJob
+    attr_reader :uai
+
     def perform(schoolings)
+      @uai = schoolings.first.establishment.uai
+
       jobs = schoolings.map { |schooling| Generate::AttributiveDecisionJob.new(schooling) }
 
       ActiveJob.perform_all_later(jobs)
+    end
+
+    private
+
+    def sync_data
+      UpdateConfirmedDirectorJob.new.perform(uai)
     end
   end
 end
