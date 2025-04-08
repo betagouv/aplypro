f4f4773b3af7f76ef8bb9f404c50e71106e29fbc
pskl <hello@pascal.cc>
2025-04-08 18:36:15 +0200

    Add basic test file

---
+++ spec/services/payment_request_reprocessor_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe PaymentRequestReprocessor do
+  let(:reason_key) { "ine_not_found" }
+  let(:validation_message) do
+    I18n.t(
+      "activerecord.errors.models.asp/payment_request.attributes.ready_state_validation.#{reason_key}"
+    )
+  end
+
+  describe "#reprocess!" do
+    let(:reprocessor) { described_class.new(reason_key) }
+
+    before do
+      create_list(:asp_payment_request, 3, :incomplete, incomplete_reason: :ine_not_found)
+    end
+
+    it "reprocesses incomplete payment requests" do
+      results = reprocessor.reprocess!
+
+      expect(results[:success] + results[:failure]).to eq(3)
+    end
+  end
+end
