76090475d889fa830189455adbf78e3070d7fc2a
pskl <hello@pascal.cc>
2025-04-23 14:42:59 +0200

    Extract CSS, make part count dynamic to handle all sizes of IBANs

--- app/javascript/controllers/iban_controller.js
+++ app/javascript/controllers/iban_controller.js
@@ -10,16 +10,15 @@ export default class extends Controller {
     buildInputs() {
         const hidden = this.hiddenInputTarget
         const value = hidden.value.replace(/\s+/g, "")
+        const numParts = Math.ceil(value.length / 4) || 7
 
         const container = document.createElement("div")
-        container.classList.add("fr-input-group", "fr-mt-1w")
-        container.style.gap = "5px"
-        container.style.display = "flex"
+        container.classList.add("fr-input-group", "fr-mt-1w", "iban-container")
 
         const lengthPerPart = 4
         this.inputFields = []
 
-        for (let i = 0; i < 7; i++) {
+        for (let i = 0; i < numParts; i++) {
             const input = document.createElement("input")
             input.type = "text"
             input.inputMode = "numeric"
@@ -27,7 +26,6 @@ export default class extends Controller {
             input.classList.add("iban-part", "fr-input", "fr-col-md-1")
             input.dataset.index = i
 
-            // Inject prefilled value
             input.value = value.slice(i * lengthPerPart, (i + 1) * lengthPerPart)
 
             input.addEventListener("input", this.handleInput.bind(this))
@@ -46,16 +44,13 @@ export default class extends Controller {
         e.preventDefault()
         const pasteData = (e.clipboardData || window.clipboardData).getData("text").replace(/\s+/g, "")
 
-        // Remplir chaque champ avec 4 caractères de la chaîne
         this.inputFields.forEach((input, i) => {
             const start = i * 4
             input.value = pasteData.slice(start, start + 4)
         })
 
-        // Met à jour le champ caché
         this.updateHiddenInput()
 
-        // Focus sur le dernier champ non vide
         const nextEmpty = this.inputFields.find(i => i.value.length < i.maxLength)
         if (nextEmpty) nextEmpty.focus()
     }
