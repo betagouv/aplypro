4e4b9906c688f9add6661d0098706c1d015ab4c4
pskl <hello@pascal.cc>
2025-05-05 13:16:14 +0200

    Put merger call outside of transaction

--- app/jobs/process_asp_response_file_job.rb
+++ app/jobs/process_asp_response_file_job.rb
@@ -2,18 +2,33 @@
 
 class ProcessASPResponseFileJob < ApplicationJob
   queue_as :payments
+  attr_reader :record
 
   def perform(raw_filename)
     filename = ASP::Filename.new(raw_filename)
 
-    record = ActiveStorage::Blob
-             .find_by!("active_storage_blobs.filename": filename.to_s)
-             .attachments
-             .first
-             .record
+    @record = ActiveStorage::Blob
+              .find_by!("active_storage_blobs.filename": filename.to_s)
+              .attachments
+              .first
+              .record
+    process_file
+  rescue ASP::Errors::IntegrationError => e
+    attempt_resolve(e)
+  end
 
-    ActiveRecord::Base.transaction do
+  private
+
+  def process_file
+    ApplicationRecord.transaction do
       record.parse_response_file!(filename.kind)
     end
+  end
+
+  def attempt_resolve(error)
+    raise error unless error.message.include?("index_students_on_asp_individu_id")
+
+    StudentMerger.new(payment_request.student.duplicates.to_a).merge!
+    process_file
   end
 end
