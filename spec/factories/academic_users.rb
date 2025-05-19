893a42b266fafbf00d3e5954547686c69cf6ee3b
pskl <hello@pascal.cc>
2025-05-19 18:12:12 +0200

    Add Etab academic singular view for overview of payment states (#1541)
    - [x] Link from academic view to Etab detail show page
    - [x] Basic show page with the same info from the academic view (will
    contain payment states)
    - [x] Return button to map

---
+++ spec/factories/academic_users.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :academic_user, class: "Academic::User" do
+    sequence(:email) { |n| "academic_user_#{n}@example.com" }
+    sequence(:uid) { |n| "academic_#{n}" }
+    name { Faker::Name.name }
+    provider { "MyString" }
+    secret { "MyString" }
+    token { "MyString" }
+  end
+end
