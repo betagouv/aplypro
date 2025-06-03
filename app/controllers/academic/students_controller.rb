7c7c9cf8377940a9e5b75e357894d193f66698fb
pskl <hello@pascal.cc>
2025-06-03 17:01:23 +0200

    Extract search fragment into partial

---
+++ app/controllers/academic/students_controller.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+module Academic
+  class StudentsController < ApplicationController
+    before_action :set_student, :check_student!, only: :show
+
+    rescue_from ActiveRecord::RecordNotFound, with: :redirect_to_class
+
+    before_action :set_search_result, only: :search_results
+
+    def show
+      @schoolings = @student.schoolings
+
+      infer_page_title(name: @student.full_name)
+    end
+
+    def search_results
+      infer_page_title
+    end
+
+    private
+
+    def set_student
+      @student = Student.find(params[:id])
+    end
+
+    def check_student!
+      raise ActiveRecord::RecordNotFound unless @student.any_classes_in_establishment?(current_establishment)
+    end
+
+    def redirect_to_class
+      redirect_to school_year_classes_path(selected_school_year), alert: t("errors.students.not_found")
+    end
+
+    def set_search_result
+      @name = params[:name]
+
+      @students = current_establishment.find_students(@name)
+    end
+  end
+end
