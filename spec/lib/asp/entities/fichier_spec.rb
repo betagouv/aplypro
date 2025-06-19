da64cd68fe327c15800a4e7f951f50d9c0cb4e3b
tnicolas1 <thibault.nicolas@ac-clermont.fr>
2025-06-19 10:18:46 +0200

    Ajout de tests unitaires

--- spec/lib/asp/entities/fichier_spec.rb
+++ spec/lib/asp/entities/fichier_spec.rb
@@ -19,14 +19,17 @@ describe ASP::Entities::Fichier do
     context "when there are multiple students" do
       let(:requests) { create_list(:asp_payment_request, 3, :ready) }
 
-      it "includes one record per schooling" do
+      it "includes one record per student" do
         expect(document / "ENREGISTREMENT").to have(3).elements
       end
     end
 
-    context "when there are multiple payments with the same schooling" do
-      let(:schooling) { create(:schooling) }
-      let(:requests) { create_list(:asp_payment_request, 3, :ready, schooling: schooling) }
+    context "when there are multiple payments for the same student" do
+      let(:student) { create(:student) }
+      let(:schooling) { create(:schooling, student: student) }
+      let(:requests) { create_list(:asp_payment_request, 3, :ready) }
+
+      before { requests.each { |p_r| p_r.pfmp.update(schooling: schooling) } }
 
       it "includes only one record" do
         expect(document / "ENREGISTREMENT").to have(1).elements
