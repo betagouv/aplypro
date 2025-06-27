8f8e52da5dea235e53ae0d56fc0a880155691e76
pskl <hello@pascal.cc>
2025-06-27 10:23:20 +0200

    Convert academic map to turbo frame

--- app/javascript/controllers/academic_controller.js
+++ app/javascript/controllers/academic_controller.js
@@ -61,6 +61,11 @@ export default class extends Controller {
   }
 
   async connect() {
+    if (!this.hasMapContainerTarget) {
+      console.warn("MapContainer target not found, skipping map initialization")
+      return
+    }
+
     this.d3 = await import("d3")
     this.d3Tile = await import("d3-tile")
 
@@ -76,7 +81,9 @@ export default class extends Controller {
   }
 
   disconnect() {
-    this.mapContainerTarget.innerHTML = ''
+    if (this.hasMapContainerTarget) {
+      this.mapContainerTarget.innerHTML = ''
+    }
   }
 
   createLegend() {
