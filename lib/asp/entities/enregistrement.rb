ce95fbb81d707b752399bf4fd8aae6c30dab1ea6
pskl <hello@pascal.cc>
2025-06-27 11:06:29 +0200

    Re-enable commented out code

--- lib/asp/entities/enregistrement.rb
+++ lib/asp/entities/enregistrement.rb
@@ -22,7 +22,7 @@ module ASP
         xml.individu(**individu_attrs) { individu(xml) }
       end
 
-      def individu(xml)
+      def individu(xml) # rubocop disable:Metrics/AbcSize # rubocop:disable Metrics/AbcSize
         xml.natureindividu("P")
         PersPhysique.from_payment_request(payment_requests.first).to_xml(xml)
         xml.adressesindividu { adresse_entity_class.from_payment_request(payment_requests.first).to_xml(xml) }
@@ -32,16 +32,14 @@ module ASP
             Dossier.from_payment_requests(grouped_p_r).to_xml(xml)
           end
         end
-
-        # TODO
-        # rescue ActiveModel::ValidationError => e
-        #   Sentry.capture_exception(
-        #     ASP::Errors::PaymentFileValidationError.new(
-        #       "Payment file validation failed for p_r: #{payment_request.id} " \
-        #       "with message #{e.message}"
-        #     )
-        #   )
-        #   raise e
+      rescue ActiveModel::ValidationError => e
+        Sentry.capture_exception(
+          ASP::Errors::PaymentFileValidationError.new(
+            "Payment file validation failed for p_r: #{payment_request.id} " \
+            "with message #{e.message}"
+          )
+        )
+        raise e
       end
 
       def individu_attrs
