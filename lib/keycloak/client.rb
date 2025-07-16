2732a38fcbb346b6371bb57205018abbef1b14a5
pskl <hello@pascal.cc>
2025-07-16 16:25:08 +0200

    Add KC Client (#1653)

---
+++ lib/keycloak/client.rb
@@ -0,0 +1,78 @@
+# frozen_string_literal: true
+
+module Keycloak
+  class Client
+    KEYCLOAK_GRANT_TYPE = "password"
+
+    def keycloak_host
+      ENV.fetch("KEYCLOAK_HOST")
+    end
+
+    def keycloak_admin
+      ENV.fetch("KEYCLOAK_ADMIN")
+    end
+
+    def keycloak_admin_password
+      ENV.fetch("KEYCLOAK_ADMIN_PASSWORD")
+    end
+
+    attr_reader :access_token, :connection
+
+    def initialize
+      @access_token = authenticate
+      @connection = build_connection
+    end
+
+    def authenticate
+      response = auth_connection.post("token", auth_form_params)
+      JSON.parse(response.body)["access_token"]
+    end
+
+    def auth_connection
+      Faraday.new(
+        url: "#{keycloak_host}/realms/master/protocol/openid-connect",
+        headers: auth_headers
+      )
+    end
+
+    def build_connection
+      Faraday.new(
+        url: "#{keycloak_host}/admin",
+        headers: headers
+      ) do |conn|
+        conn.request :json
+        conn.response :json
+      end
+    end
+
+    def auth_headers
+      { "Content-Type" => "application/x-www-form-urlencoded" }
+    end
+
+    def headers
+      {
+        "Authorization" => "Bearer #{access_token}",
+        "Content-Type" => "application/json"
+      }
+    end
+
+    def auth_form_params
+      {
+        grant_type: KEYCLOAK_GRANT_TYPE,
+        client_id: "admin-cli",
+        username: keycloak_admin,
+        password: keycloak_admin_password
+      }
+    end
+
+    def list_realms
+      response = connection.get("realms")
+      response.body
+    end
+
+    def delete_user(realm_name, user_id)
+      response = connection.delete("realms/#{realm_name}/users/#{user_id}")
+      response.body
+    end
+  end
+end
