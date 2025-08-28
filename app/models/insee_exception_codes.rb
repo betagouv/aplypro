d6aba66502a6732a6e03686e5a812ced130862d1
tnicolas1 <thibault.nicolas@ac-clermont.fr>
2025-08-28 10:01:21 +0200

    Ajout de la méthode 'transform_insee_code' dans le modèle 'insee_exception_codes'

--- app/models/insee_exception_codes.rb
+++ app/models/insee_exception_codes.rb
@@ -2,5 +2,17 @@
 
 class InseeExceptionCodes < ApplicationRecord
   validates :code_type, :entry_code, :exit_code, presence: true
-  validates :entry_code, unique: true
+  validates :entry_code, uniqueness: true
+
+  validates :entry_code, :exit_code, length: { maximum: 5 }, allow_blank: true
+
+  class << self
+    def transform_insee_code(entry_code, code_type = "address")
+      exception_code = InseeExceptionCodes.find_by(entry_code:, code_type:)
+
+      return exception_code.exit_code if exception_code.present?
+
+      entry_code
+    end
+  end
 end
--- spec/services/asp/mappers/adresse/france_mapper_spec.rb
+++ spec/services/asp/mappers/adresse/france_mapper_spec.rb
@@ -8,12 +8,6 @@ describe ASP::Mappers::Adresse::FranceMapper do
   let(:payment_request) { create(:asp_payment_request) }
   let(:student) { payment_request.student }
 
-  described_class::MAPPING.each do |name, mapping|
-    it "maps to the student's`#{mapping}'" do
-      expect(mapper.send(name)).to eq student[mapping]
-    end
-  end
-
   describe "codeinseepays" do
     subject(:code) { mapper.codeinseepays }
 
@@ -22,6 +16,18 @@ describe ASP::Mappers::Adresse::FranceMapper do
     end
 
     it "delegates to the INSEE country code mapper" do
+      expect(code).to eq :value
+    end
+  end
+
+  describe "codecominsee" do
+    subject(:code) { mapper.codecominsee }
+
+    before do
+      allow(InseeExceptionCodes).to receive(:transform_insee_code).and_return :value
+    end
+
+    it "delegates to the INSEE exception codes transformer" do
       expect(code).to eq :value
     end
   end
