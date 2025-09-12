c9a1ef0116c514ed32c10462a00875caadc5f2d2
pskl <hello@pascal.cc>
2025-09-12 11:14:00 +0200

    Fetch date ranges from calendrier scolaire provided by data gouv API

---
+++ app/services/date_range_fetcher.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+# Returns the eligible range that is allowed for establishments to input PFMPs for a given year based on API data
+class DateRangeFetcher
+  BASE_URL = "https://data.education.gouv.fr/api/v2/catalog/datasets/fr-en-calendrier-scolaire"
+
+  class << self
+    def call(academy_code, year = SchoolYear.current.start_year) # rubocop:disable Metrics/AbcSize
+      location = Establishment::ACADEMY_LABELS.fetch(academy_code)
+
+      previous_year = SchoolYear.new(start_year: year - 1).to_s
+      current_year = SchoolYear.new(start_year: year).to_s
+
+      records = fetch_summer_vacations_for_location_and_years(location, previous_year, current_year)
+
+      raise "No summer vacation records found for #{location}" if records.empty?
+
+      start_date = Date.parse(find_summer_vacation_for_year(records, previous_year)[:end_date]) + 1
+      end_date = Date.parse(find_summer_vacation_for_year(records, current_year)[:end_date])
+      (start_date..end_date)
+    end
+
+    private
+
+    def fetch_summer_vacations_for_location_and_years(location, previous_year, current_year)
+      vacation_type = location == "Réunion" ? "Vacances d'Hiver austral" : "Vacances d'Été"
+      where_clause = "location=\"#{location}\" AND (annee_scolaire=\"#{previous_year}\" OR " \
+                     "annee_scolaire=\"#{current_year}\") AND description=\"#{vacation_type}\" AND population=\"Élèves\""
+      query_params = {
+        where: where_clause,
+        select: "description,start_date,end_date,location,annee_scolaire,zones,population",
+        limit: 100
+      }
+
+      response = client.get("#{BASE_URL}/records", query_params)
+      data = JSON.parse(response.body)
+      data["records"] || []
+    rescue Faraday::Error, JSON::ParserError
+      []
+    end
+
+    def find_summer_vacation_for_year(records, school_year)
+      year_record = records.find do |record|
+        record.dig("record", "fields", "annee_scolaire") == school_year
+      end
+
+      return nil if year_record.nil?
+
+      {
+        end_date: year_record.dig("record", "fields", "end_date")
+      }
+    end
+
+    def client
+      @client ||= Faraday.new do |f|
+        f.response :raise_error
+        f.adapter Faraday.default_adapter
+      end
+    end
+  end
+end
