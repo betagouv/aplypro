30ea544a7233756439b2f195822f7539a39f8fd9
tnicolas1 <thibault.nicolas@ac-clermont.fr>
2025-10-01 11:23:52 +0200

    Déplacement des tests de 'EstablishmentJobSpec' vers 'EstablishmentApiSpec'

--- spec/jobs/sync/establishment_job_spec.rb
+++ spec/jobs/sync/establishment_job_spec.rb
@@ -28,28 +28,4 @@ RSpec.describe Sync::EstablishmentJob do
       expect { described_class.perform_now(establishment) }.to change(establishment, attr)
     end
   end
-
-  context "when the EstablishmentApi returns no data" do
-    let(:json) { { "results" => [{}] }.to_json }
-
-    it "doesn't raise an error" do
-      expect { described_class.perform_now(establishment) }.not_to raise_error
-    end
-  end
-
-  context "when the EstablishmentApi more than one result and one of them has 'voie_professionnelle' equal to 1" do
-    let(:json) { { "results" => [{ voie_professionnelle: "1" }, { voie_professionnelle: "0" }] }.to_json }
-
-    it "raise an error" do
-      expect { described_class.perform_now(establishment) }.not_to raise_error
-    end
-  end
-
-  context "when the EstablishmentApi more than one result and two of them has 'voie_professionnelle' equal to 1" do
-    let(:json) { { "results" => [{ voie_professionnelle: "1" }, { voie_professionnelle: "1" }] }.to_json }
-
-    it "raise an error" do
-      expect { described_class.perform_now(establishment) }.to raise_error(/there are more than one establishment/)
-    end
-  end
 end
