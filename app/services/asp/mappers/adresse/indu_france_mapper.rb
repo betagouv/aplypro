2e71696693abcd086685f0975694853cab09b5be
pskl <hello@pascal.cc>
2025-10-15 15:43:06 +0200

    Update mappers to use abbreviator

--- app/services/asp/mappers/adresse/indu_france_mapper.rb
+++ app/services/asp/mappers/adresse/indu_france_mapper.rb
@@ -5,11 +5,20 @@ module ASP
     module Adresse
       class InduFranceMapper < FranceMapper
         def libellevoie
-          student.address_line1.presence || student.address_line2
+          address_line = student.address_line1.presence || student.address_line2
+          AddressAbbreviator.abbreviate(
+            address_line,
+            max_length: Entities::Adresse::InduFrance::LIBELLEVOIE_MAX_LENGTH
+          )
         end
 
         def cpltdistribution
-          student.address_line1.present? ? student.address_line2 : nil
+          return nil if student.address_line1.blank?
+
+          AddressAbbreviator.abbreviate(
+            student.address_line2,
+            max_length: Entities::Adresse::InduFrance::ADRESSE_ATTR_MAX_LENGTH
+          )
         end
       end
     end
