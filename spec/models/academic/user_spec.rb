c897318834c59be3f44db3416ad8b4bb84211d81
pskl <hello@pascal.cc>
2025-10-16 14:26:08 +0200

    Spec admin user

--- spec/models/academic/user_spec.rb
+++ spec/models/academic/user_spec.rb
@@ -10,4 +10,80 @@ RSpec.describe Academic::User do
   end
 
   it_behaves_like "a trackable user", :academic_user
+
+  describe "#admin?" do
+    subject(:academic_user) { build(:academic_user, oidc_attributes: oidc_attributes) }
+
+    context "when oidc_attributes are blank" do
+      let(:oidc_attributes) { nil }
+
+      it "returns false" do
+        expect(academic_user.admin?).to be false
+      end
+    end
+
+    context "when AplyproAcademieResp contains a star" do
+      let(:oidc_attributes) do
+        {
+          "extra" => {
+            "raw_info" => {
+              "AplyproAcademieResp" => ["*"]
+            }
+          }
+        }
+      end
+
+      it "returns true" do
+        expect(academic_user.admin?).to be true
+      end
+    end
+
+    context "when AplyproAcademieResp contains multiple academies including a star" do
+      let(:oidc_attributes) do
+        {
+          "extra" => {
+            "raw_info" => {
+              "AplyproAcademieResp" => ["01", "*", "02"]
+            }
+          }
+        }
+      end
+
+      it "returns true" do
+        expect(academic_user.admin?).to be true
+      end
+    end
+
+    context "when AplyproAcademieResp does not contain a star" do
+      let(:oidc_attributes) do
+        {
+          "extra" => {
+            "raw_info" => {
+              "AplyproAcademieResp" => ["01", "02"]
+            }
+          }
+        }
+      end
+
+      it "returns false" do
+        expect(academic_user.admin?).to be false
+      end
+    end
+
+    context "when AplyproAcademieResp is empty" do
+      let(:oidc_attributes) do
+        {
+          "extra" => {
+            "raw_info" => {
+              "AplyproAcademieResp" => []
+            }
+          }
+        }
+      end
+
+      it "returns false" do
+        expect(academic_user.admin?).to be false
+      end
+    end
+  end
 end
