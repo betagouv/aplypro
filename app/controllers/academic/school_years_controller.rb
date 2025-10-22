bd9f95ebd8096dcd98864d43eb1a31650fe212ca
pskl <hello@pascal.cc>
2025-10-22 12:54:08 +0200

    Check existence of SC before redirect

--- app/controllers/academic/school_years_controller.rb
+++ app/controllers/academic/school_years_controller.rb
@@ -7,9 +7,12 @@ module Academic
     def selected
       start_year = params[:school_year_id]
 
-      session[:start_year] = start_year
-
-      redirect_to academic_home_path
+      if SchoolYear.exists?(start_year: start_year)
+        session[:start_year] = start_year
+        redirect_to academic_home_path
+      else
+        redirect_to select_academic_school_years_path, alert: t(".invalid_school_year")
+      end
     end
   end
 end
