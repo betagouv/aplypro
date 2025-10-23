1875c746463fc748cb504c0d2b20a912289eb12f
pskl <hello@pascal.cc>
2025-10-23 17:20:00 +0200

    Remove useless method

--- app/services/reports/csv_exporter.rb
+++ app/services/reports/csv_exporter.rb
@@ -29,14 +29,10 @@ module Reports
       return "0" if cell.nil?
       return "Infini" if cell.respond_to?(:infinite?) && cell.infinite?
       return "0" if cell.respond_to?(:nan?) && cell.nan?
-      return handle_special_values(cell) unless valid_numeric?(cell)
+      return cell.to_s unless valid_numeric?(cell)
 
       rounded_value = round_value(cell)
       rounded_value.to_s.gsub(".", ",")
-    end
-
-    def handle_special_values(value)
-      value.to_s
     end
 
     def valid_numeric?(value)
