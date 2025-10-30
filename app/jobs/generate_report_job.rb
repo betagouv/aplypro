c8fde85c9c5e160466db244b2ff976aa0f5b3bfd
pskl <hello@pascal.cc>
2025-10-30 16:05:10 +0100

    Re-add queue name to generate report job

--- app/jobs/generate_report_job.rb
+++ app/jobs/generate_report_job.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class GenerateReportJob < ApplicationJob
+  queue_as :payments
+
   def perform(school_year = SchoolYear.current, date = Time.current)
     Report.create_for_school_year(school_year, date)
 
