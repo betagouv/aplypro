1578d45c786abfc7795037764fc9c71f24c43b7a
pskl <hello@pascal.cc>
2025-11-04 17:55:44 +0100

    Display titles instead of symbols

--- app/helpers/academic/stats_helper.rb
+++ app/helpers/academic/stats_helper.rb
@@ -16,6 +16,34 @@ module Academic
       end
     end
 
+    def cell_background_color(value)
+      return "" unless value.is_a?(Numeric) && ratio_value?(value)
+
+      if value >= 0.8
+        "background-color: var(--success-950-100);"
+      elsif value >= 0.5
+        "background-color: var(--yellow-tournesol-975-75);"
+      elsif value >= 0.2
+        "background-color: var(--yellow-tournesol-850-200);"
+      else
+        "background-color: var(--red-marianne-950-100);"
+      end
+    end
+
+    def visible_columns(data)
+      return [] if data.empty?
+
+      headers = data.first
+      (0...headers.length).reject { |index| column_empty?(data, index) }
+    end
+
+    def format_table_header(key)
+      return key.to_s unless key.is_a?(Symbol) || key.is_a?(String)
+
+      symbol_key = key.is_a?(Symbol) ? key : key.to_sym
+      I18n.t("activerecord.attributes.report.#{symbol_key}", default: symbol_key.to_s.humanize)
+    end
+
     private
 
     def handle_special_values(value)
@@ -42,32 +70,11 @@ module Academic
       value.is_a?(Integer) || (value.is_a?(Numeric) && value == value.to_i)
     end
 
-    def cell_background_color(value)
-      return "" unless value.is_a?(Numeric) && ratio_value?(value)
-
-      if value >= 0.8
-        "background-color: var(--success-950-100);"
-      elsif value >= 0.5
-        "background-color: var(--yellow-tournesol-975-75);"
-      elsif value >= 0.2
-        "background-color: var(--yellow-tournesol-850-200);"
-      else
-        "background-color: var(--red-marianne-950-100);"
-      end
-    end
-
     def column_empty?(data, column_index)
       data[1..].all? do |row|
         value = row[column_index]
         value.nil? || (value.respond_to?(:nan?) && value.nan?)
       end
-    end
-
-    def visible_columns(data)
-      return [] if data.empty?
-
-      headers = data.first
-      (0...headers.length).reject { |index| column_empty?(data, index) }
     end
   end
 end
