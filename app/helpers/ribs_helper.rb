bc1e097a9473cc5e765f059f7b101b52f27903fb
tnicolas1 <thibault.nicolas@ac-clermont.fr>
2025-11-06 09:31:30 +0100

    Floutage de l'IBAN plutôt que de remplacer par des '*'

--- app/helpers/ribs_helper.rb
+++ app/helpers/ribs_helper.rb
@@ -8,17 +8,4 @@ module RibsHelper
   def format_iban(iban)
     iban&.gsub(/(.{4})/, '\1 ')
   end
-
-  def hide_format_iban(iban)
-    return "" if iban.blank?
-
-    raw = iban.delete(" ")
-
-    visible_start = 4
-    visible_end = 3
-    masked_length = raw.length - visible_start - visible_end
-    masked = raw[0, visible_start] + ("*" * masked_length) + raw[-visible_end, visible_end]
-
-    format_iban(masked)
-  end
 end
--- app/javascript/controllers/hide_iban_controller.js
+++ app/javascript/controllers/hide_iban_controller.js
@@ -4,13 +4,11 @@ export default class extends Controller {
     static targets = ["text"]
 
     connect() {
-        this.hidden = true
+        this.isBlurred = true
     }
 
     toggle() {
-        const hide = this.textTarget.dataset.hide
-        const full = this.textTarget.dataset.full
-        this.textTarget.textContent = this.hidden ? full : hide
-        this.hidden = !this.hidden
+        this.isBlurred = !this.isBlurred
+        this.textTarget.classList.toggle("iban-blur", this.isBlurred)
     }
 }
