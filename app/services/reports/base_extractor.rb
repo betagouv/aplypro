934f67555947a8d8f22608ef0c7095aa5b9e687d
pskl <hello@pascal.cc>
2025-11-07 16:13:02 +0100

    Cache extractions in Redis

--- app/services/reports/base_extractor.rb
+++ app/services/reports/base_extractor.rb
@@ -7,14 +7,24 @@ module Reports
     def initialize(report)
       @report = report
       validate_report_not_loaded!
-      @cache = {}
     end
 
     def extract(*keys)
-      keys_to_fetch = keys.reject { |key| @cache.key?(key) }
-      fetch_from_database(keys_to_fetch) if keys_to_fetch.any?
+      results = {}
+      uncached_keys = []
 
-      keys.size == 1 ? @cache[keys.first] : @cache.slice(*keys)
+      keys.each do |key|
+        cached_value = Rails.cache.read(cache_key(key))
+        if cached_value
+          results[key] = cached_value
+        else
+          uncached_keys << key
+        end
+      end
+
+      results.merge!(fetch_from_database(uncached_keys)) if uncached_keys.any?
+
+      keys.size == 1 ? results[keys.first] : results.slice(*keys)
     end
 
     def calculate_stats
@@ -34,9 +44,23 @@ module Reports
     end
 
     def fetch_from_database(keys)
+      return {} if keys.empty?
+
       select_clauses = keys.map { |key| "data -> '#{key}' as #{key}" }.join(", ")
       result = @report.class.select(select_clauses).find(@report.id)
-      keys.each { |key| @cache[key] = result.public_send(key) }
+
+      fetched_data = {}
+      keys.each do |key|
+        value = result.public_send(key)
+        Rails.cache.write(cache_key(key), value, expires_in: 1.week)
+        fetched_data[key] = value
+      end
+
+      fetched_data
+    end
+
+    def cache_key(key)
+      "reports/#{@report.id}/#{key}"
     end
 
     def extract_data_row
