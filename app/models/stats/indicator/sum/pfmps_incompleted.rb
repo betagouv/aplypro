89d31f83b45700d732e4644ae09336fbceebcb0e
pskl <hello@pascal.cc>
2025-11-20 14:36:29 +0100

    Calculate theoritical amount based on start_date + end_date for incompleted pfmps indicator

--- app/models/stats/indicator/sum/pfmps_incompleted.rb
+++ app/models/stats/indicator/sum/pfmps_incompleted.rb
@@ -34,12 +34,53 @@ module Stats
           "stats.sum.pfmps_incompleted"
         end
 
+        def global_data
+          calculate_theoretical_sum(all)
+        end
+
+        def bops_data
+          @bops_data ||= calculate_theoretical_sum(group_per_bop(all))
+                         .transform_keys { |bop| bop_key_map(bop) }
+        end
+
+        def menj_academies_data
+          @menj_academies_data ||= calculate_theoretical_sum(group_per_menj_academy(all))
+        end
+
+        def establishments_data
+          @establishments_data ||= calculate_theoretical_sum(group_per_establishment(all))
+        end
+
         def with_mef_and_establishment
           Pfmp.joins(schooling: { classe: %i[mef establishment school_year] })
         end
 
         def with_establishment
           Pfmp.joins(schooling: { classe: %i[establishment school_year] })
+        end
+
+        private
+
+        def calculate_theoretical_sum(pfmps_relation)
+          is_grouped = pfmps_relation.group_values.any?
+
+          base_relation = if is_grouped
+                            pfmps_relation.except(:distinct)
+                          else
+                            Pfmp.where(id: pfmps_relation.select(:id).distinct)
+                          end
+
+          result = base_relation
+                   .joins(schooling: { classe: :mef })
+                   .joins("INNER JOIN wages ON wages.mefstat4 = LEFT(mefs.mefstat11, 4) " \
+                          "AND wages.ministry = mefs.ministry " \
+                          "AND wages.school_year_id = mefs.school_year_id")
+                   .sum("ROUND(((pfmps.end_date - pfmps.start_date)::integer * 5.0 / 7.0)::numeric, 0) " \
+                        "* wages.daily_rate")
+
+          return result.to_i if result.is_a?(Numeric)
+
+          result.transform_values(&:to_i)
         end
       end
     end
--- spec/models/stats/indicator/sum/pfmps_incompleted_spec.rb
+++ spec/models/stats/indicator/sum/pfmps_incompleted_spec.rb
@@ -1,14 +1,116 @@
 # frozen_string_literal: true
 
 require "rails_helper"
-require "./spec/models/stats/shared_contexts"
 
 describe Stats::Indicator::Sum::PfmpsIncompleted do
-  describe "#menj_academies_data" do
-    subject { described_class.new(SchoolYear.current.start_year).menj_academies_data }
+  let(:current_start_year) { SchoolYear.current.start_year }
+  let(:current_school_year) { SchoolYear.current }
 
-    include_context "when there is data for stats per MENJ academies"
+  describe "#global_data" do
+    subject(:result) { described_class.new(current_start_year).global_data }
 
-    it { is_expected.to be_a(Hash) }
+    context "with incomplete PFMPs" do
+      before do
+        mef = create(:mef, daily_rate: 10, yearly_cap: 500)
+        establishment = create(:establishment)
+        classe = create(:classe, mef: mef, establishment: establishment, school_year: current_school_year)
+        school_year_range = establishment.school_year_range(current_start_year)
+
+        student = create(:student, :asp_ready, establishment: establishment)
+        schooling = create(:schooling, :with_attributive_decision, classe: classe, student: student)
+        schooling.update!(start_date: school_year_range.first, end_date: school_year_range.last)
+
+        create(:pfmp, schooling: schooling, amount: 999,
+                      start_date: school_year_range.first + 1.day,
+                      end_date: school_year_range.first + 8.days)
+      end
+
+      it "calculates theoretical amount using (duration * 5/7) * daily_rate" do
+        duration = 7
+        expected_workdays = (duration * 5.0 / 7.0).round
+        expected_amount = expected_workdays * 10
+
+        expect(result).to eq(expected_amount)
+      end
+    end
+
+    context "with multiple incomplete PFMPs" do
+      before do
+        mef = create(:mef, daily_rate: 15, yearly_cap: 500)
+        establishment = create(:establishment)
+        classe = create(:classe, mef: mef, establishment: establishment, school_year: current_school_year)
+        school_year_range = establishment.school_year_range(current_start_year)
+
+        2.times do
+          student = create(:student, :asp_ready, establishment: establishment)
+          schooling = create(:schooling, :with_attributive_decision, classe: classe, student: student)
+          schooling.update!(start_date: school_year_range.first, end_date: school_year_range.last)
+
+          create(:pfmp, schooling: schooling, amount: 100,
+                        start_date: school_year_range.first + 1.day,
+                        end_date: school_year_range.first + 15.days)
+        end
+      end
+
+      it "sums theoretical amounts correctly" do
+        duration = 14
+        expected_workdays = (duration * 5.0 / 7.0).round
+        expected_total = (expected_workdays * 15) * 2
+
+        expect(result).to eq(expected_total)
+      end
+    end
+
+    context "when PFMP is validated" do
+      before do
+        mef = create(:mef, daily_rate: 10, yearly_cap: 500)
+        establishment = create(:establishment)
+        classe = create(:classe, mef: mef, establishment: establishment, school_year: current_school_year)
+        school_year_range = establishment.school_year_range(current_start_year)
+
+        student = create(:student, :asp_ready, establishment: establishment)
+        schooling = create(:schooling, :with_attributive_decision, classe: classe, student: student)
+        schooling.update!(start_date: school_year_range.first, end_date: school_year_range.last)
+
+        create(:pfmp, :validated, schooling: schooling, amount: 100,
+                                  start_date: school_year_range.first + 1.day,
+                                  end_date: school_year_range.first + 8.days)
+      end
+
+      it "excludes validated PFMPs" do
+        expect(result).to eq(0)
+      end
+    end
+  end
+
+  describe "#bops_data" do
+    subject(:bops_result) { described_class.new(current_start_year).bops_data }
+
+    before do
+      establishment = create(:establishment)
+
+      Mef.ministries.each_key.with_index do |ministry, index|
+        mef = create(:mef, ministry: ministry, daily_rate: 10, yearly_cap: 500)
+        school_year_range = establishment.school_year_range(current_start_year)
+        classe = create(:classe, mef: mef, establishment: establishment, school_year: current_school_year)
+
+        (index + 1).times do
+          student = create(:student, :asp_ready, establishment: establishment)
+          schooling = create(:schooling, :with_attributive_decision, classe: classe, student: student)
+          schooling.update!(start_date: school_year_range.first, end_date: school_year_range.last)
+
+          create(:pfmp, schooling: schooling, amount: 100,
+                        start_date: school_year_range.first + 1.day,
+                        end_date: school_year_range.first + 8.days)
+        end
+      end
+    end
+
+    it "groups theoretical amounts by BOP" do
+      workdays = (7 * 5.0 / 7.0).round
+      amount = workdays * 10
+
+      expect(bops_result).to include("ENPU" => amount, "MASA" => amount * 2)
+    end
   end
 end
