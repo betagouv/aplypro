1df8f6d74d6ebe9d8593b8aa1d34bff016ea10d5
pskl <hello@pascal.cc>
2026-02-06 14:21:16 +0100

    Replace codeobjet markup with codecheance markup with start date of pfmp formatted

--- lib/asp/entities/element_paiement.rb
+++ lib/asp/entities/element_paiement.rb
@@ -3,15 +3,15 @@
 module ASP
   module Entities
     class ElementPaiement < Entity
-      attribute :codeobjet, :string
+      attribute :objetecheance, :string
       attribute :codetypeversement, :string
       attribute :mttotalfinancement, :string
       attribute :usprinc, :string
 
-      validates_presence_of %i[codeobjet codetypeversement mttotalfinancement usprinc]
+      validates_presence_of %i[objetecheance codetypeversement mttotalfinancement usprinc]
 
       def fragment(xml)
-        xml.codeobjet(codeobjet)
+        xml.objetecheance(objetecheance)
         xml.listeversement do
           xml.versement do
             xml.codetypeversement(codetypeversement)
