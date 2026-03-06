0cc1b7c7a26a97d87b175d34e4b4fa424f97c5f1
pskl <hello@pascal.cc>
2026-03-06 16:59:44 +0100

    Add CorrectionEtranger

--- lib/asp/entities/adresse/correction_france.rb
+++ lib/asp/entities/adresse/correction_france.rb
@@ -10,22 +10,37 @@ module ASP
         attribute :codetypevoie, :string
         attribute :cpltdistribution, :string
 
-        validates_presence_of :libellevoie
+        validates_presence_of %i[
+          libellevoie
+          codetypeadr
+          codeinseepays
+          codepostalcedex
+          codecominsee
+        ]
+
+        validates_length_of :libellevoie, maximum: InduFrance::LIBELLEVOIE_MAX_LENGTH
+        validates_length_of :cpltdistribution, maximum: InduFrance::ADRESSE_ATTR_MAX_LENGTH, allow_nil: true
 
         def self.payment_mapper_class
           Mappers::Adresse::CorrectionFranceMapper
         end
 
         def fragment(xml)
+          voie_fragment(xml)
+          xml.codetypeadr(codetypeadr)
+          xml.codeinseepays(codeinseepays)
+          xml.codepostalcedex(codepostalcedex)
+          xml.codecominsee(codecominsee)
+        end
+
+        private
+
+        def voie_fragment(xml)
           xml.numerovoie(numerovoie) if numerovoie.present?
           xml.libellevoie(libellevoie)
           xml.codeextensionvoie(codeextensionvoie) if codeextensionvoie.present?
           xml.codetypevoie(codetypevoie) if codetypevoie.present?
           xml.cpltdistribution(cpltdistribution) if cpltdistribution.present?
-          xml.codetypeadr(codetypeadr)
-          xml.codeinseepays(codeinseepays)
-          xml.codepostalcedex(codepostalcedex)
-          xml.codecominsee(codecominsee)
         end
       end
     end
