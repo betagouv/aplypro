1d1925b24fe797fa4d0da124b6ab5de95c595235
pskl <hello@pascal.cc>
2026-04-16 06:44:36 +0200

    Clean up abbreviator

--- app/services/asp/address_abbreviator.rb
+++ app/services/asp/address_abbreviator.rb
@@ -22,23 +22,19 @@ module ASP
       end
 
       def abbreviate_address_line(text, max_length:)
-        abbreviate(text, max_length: max_length, csv_path: COMMON_NAMES_ABBREVIATIONS_PATH)
-      end
-
-      private
-
-      def abbreviate(text, max_length:, csv_path:)
         return nil if text.blank?
         return text if text.length <= max_length
 
         abbreviated_text = normalize(text.dup)
 
-        load_abbreviations(csv_path).each do |full_word, abbreviation|
+        load_abbreviations(COMMON_NAMES_ABBREVIATIONS_PATH).each do |full_word, abbreviation|
           abbreviated_text.gsub!(/\b#{Regexp.escape(full_word)}\b/i, abbreviation)
         end
 
         abbreviated_text
       end
+
+      private
 
       def load_abbreviations(csv_path)
         Rails.cache.fetch("abbreviations_cache/#{csv_path}", expires_in: 3.hours) do
