63df4cfd04f5ca7fd1e1945a43b4c4e71556ef6a
pskl <hello@pascal.cc>
2026-05-08 09:58:52 +0200

    Add feature specs for pagination on reports

--- features/step_definitions/academic_steps.rb
+++ features/step_definitions/academic_steps.rb
@@ -23,3 +23,28 @@ Alors("je devrais voir l'académie {string} dans les options") do |academy_code|
   academy_label = Establishment::ACADEMY_LABELS[academy_code]
   expect(page).to have_content("#{academy_label} (#{academy_code})")
 end
+
+Sachantque("il existe {int} rapports") do |count|
+  school_year = SchoolYear.first || FactoryBot.create(:school_year)
+  count.times { FactoryBot.create(:report, school_year: school_year) }
+end
+
+Quand("je me rends sur la page des rapports") do
+  visit "/academic/reports"
+end
+
+Alors("l'URL contient {string}") do |fragment|
+  expect(current_url).to include(fragment)
+end
+
+Alors("la page contient un lien vers la page de pagination {int}") do |page_number|
+  expect(page).to have_link(page_number.to_s, href: /page=#{page_number}/)
+end
+
+Alors("la page ne contient pas de lien vers la page de pagination {int}") do |page_number|
+  expect(page).to have_no_link(page_number.to_s, href: /page=#{page_number}/)
+end
+
+Quand("je clique sur le lien de pagination {int}") do |page_number|
+  click_link(page_number.to_s, href: /page=#{page_number}/)
+end
